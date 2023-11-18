7e1b19e54d2be2958b1fef98d52b4860567d5f40
HanCosta <hancosta@outlook.com>
2023-11-18 12:31:05 +0800

    ctrl enter will not into new path now

--- ContentPanel.h
+++ ContentPanel.h
@@ -10,59 +10,56 @@
 #include "View/DragDropTableView.h"
 
 class ContentPanel : public QWidget {
-    Q_OBJECT
-public:
-    explicit ContentPanel(QWidget* parent = nullptr,
-                         const QString& defaultPath = "",
-                         FolderPreviewHTML* previewHtml_ = nullptr,
-                         FolderPreviewWidget* previewWidget_ = nullptr,
-                         CustomStatusBar* _statusBar = nullptr);
-    auto CurrentPath() -> QString {
-        if (!fileSysModel) {
-            qDebug("[Error] fileSysModel is nullptr");
-            return "";
-        }
-        return QFileInfo(fileSysModel->rootPath()).absoluteFilePath();
+  Q_OBJECT
+ public:
+  explicit ContentPanel(QWidget* parent = nullptr,
+                        const QString& defaultPath = "",
+                        FolderPreviewHTML* previewHtml_ = nullptr,
+                        FolderPreviewWidget* previewWidget_ = nullptr,
+                        CustomStatusBar* _statusBar = nullptr);
+  auto CurrentPath() -> QString {
+    if (!fileSysModel) {
+      qDebug("[Error] fileSysModel is nullptr");
+      return "";
     }
+    return QFileInfo(fileSysModel->rootPath()).absoluteFilePath();
+  }
 
-    auto IntoNewPath(QString newPath,
-                     bool isNewPath = true,
-                     bool isF5Force = false) -> bool;
+  auto IntoNewPath(QString newPath, bool isNewPath = true, bool isF5Force = false) -> bool;
 
-    auto on_searchTextChanged(const QString& targetStr) -> bool;
-    auto on_searchEnterKey(const QString& targetStr) -> bool;
+  auto on_searchTextChanged(const QString& targetStr) -> bool;
+  auto on_searchEnterKey(const QString& targetStr) -> bool;
 
-    auto subscribe() -> void;
-    auto on_cellDoubleClicked(QModelIndex clickedIndex) -> bool;
-    auto on_selectionChanged(const QItemSelection& selected,
-                             const QItemSelection& deselected) -> bool;
-    auto onAfterDirectoryLoaded(const QString& loadedPath) -> bool;
+  auto subscribe() -> void;
+  auto on_cellDoubleClicked(QModelIndex clickedIndex) -> bool;
+  auto on_selectionChanged(const QItemSelection& selected, const QItemSelection& deselected) -> bool;
+  auto onAfterDirectoryLoaded(const QString& loadedPath) -> bool;
 
-    auto keyPressEvent(QKeyEvent* e) -> void override{
-        if (e->key() == Qt::Key_Backspace) {
-            addressBar->onBackspaceEvent();
-            return;
-        } else if (e->key() == Qt::Key_Enter or e->key() == Qt::Key_Return) {
-            on_cellDoubleClicked(view->currentIndex());
-            return;
-        }
-        QWidget::keyPressEvent(e);
+  auto keyPressEvent(QKeyEvent* e) -> void override {
+    if (e->modifiers() == Qt::NoModifier and e->key() == Qt::Key_Backspace) {
+      addressBar->onBackspaceEvent();
+      return;
+    } else if (e->modifiers() == Qt::NoModifier and (e->key() == Qt::Key_Enter or e->key() == Qt::Key_Return)) {
+      on_cellDoubleClicked(view->currentIndex());
+      return;
     }
+    QWidget::keyPressEvent(e);
+  }
 
-signals:
+ signals:
 
-public:
-    struct Anchor {
-        int row;
-        int col;
-    };
-    QMap<QString, Anchor> m_anchorTags;
-    MyQFileSystemModel* fileSysModel;
-    NavigationAndAddressBar* addressBar;
-    DragDropTableView* view;
-    FolderPreviewHTML* previewHtml;
-    FolderPreviewWidget* previewWidget;
-    CustomStatusBar* logger;
+ public:
+  struct Anchor {
+    int row;
+    int col;
+  };
+  QMap<QString, Anchor> m_anchorTags;
+  MyQFileSystemModel* fileSysModel;
+  NavigationAndAddressBar* addressBar;
+  DragDropTableView* view;
+  FolderPreviewHTML* previewHtml;
+  FolderPreviewWidget* previewWidget;
+  CustomStatusBar* logger;
 };
 
 #endif  // CONTENTPANEL_H
