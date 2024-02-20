0a79247b6dc3d612a066f3db6cc7075fa92c95d3
Ariel <hancosta@outlook.com>
2024-02-20 10:22:42 -0500

    Version 41.8 Copied/Cut/Paste Illustration

--- MyQFileSystemModel.h
+++ MyQFileSystemModel.h
@@ -9,78 +9,109 @@
 #include "Component/CustomStatusBar.h"
 
 class MyQFileSystemModel : public QFileSystemModel {
-public:
-    explicit MyQFileSystemModel(CustomStatusBar* _statusBar = nullptr, QObject* parent = nullptr);
+ public:
+  explicit MyQFileSystemModel(CustomStatusBar* _statusBar = nullptr, QObject* parent = nullptr);
 
-    Qt::ItemFlags flags(const QModelIndex& index) const override;
+  Qt::ItemFlags flags(const QModelIndex& index) const override;
 
-    bool canDropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column, const QModelIndex& parent) const override;
-    bool dropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column, const QModelIndex& parent) override;
-    Qt::DropActions supportedDropActions() const override;
-    Qt::DropActions supportedDragActions() const override;
+  bool canDropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column, const QModelIndex& parent) const override;
+  bool dropMimeData(const QMimeData* data, Qt::DropAction action, int row, int column, const QModelIndex& parent) override;
+  Qt::DropActions supportedDropActions() const override;
+  Qt::DropActions supportedDragActions() const override;
 
-    void CutSomething(const QModelIndexList& cutIndexes, bool appendMode = false) {
-        if (not appendMode) {
-            m_cutMap.clear();
-        }
-    }
-    void CopiedSomething(const QModelIndexList& copiedIndexes, bool appendMode = false) {
-        if (not appendMode) {
-            m_copiedMap.clear();
-        }
-        if (not m_copiedMap.contains(rootPath())) {
-            m_copiedMap[rootPath()] = {};
-        }
-        m_copiedMap[rootPath()] += copiedIndexes;
-    }
+  void ClearCopyAndCutDict() {
+    ClearCutDict();
+    ClearCopiedDict();
+  }
 
-    void DragHover(const QModelIndex index){
-        if (m_draggedHoverIndex.row() == index.row() and m_draggedHoverIndex.column() == index.column()){
-            return;
-        }
-        emit dataChanged(m_draggedHoverIndex, m_draggedHoverIndex, {Qt::ItemDataRole::BackgroundRole});
+  void ClearCutDict() {
+    decltype(m_cutMap) tmp;
+    tmp.swap(m_cutMap);
+    for (auto it = tmp.cbegin(); it != tmp.cend(); ++it) {
+      for (auto index : it.value()) {
         emit dataChanged(index, index, {Qt::ItemDataRole::BackgroundRole});
-        m_draggedHoverIndex = index;
+      }
     }
+  }
 
-    void DragRelease(const QModelIndex index=QModelIndex()){
-        emit dataChanged(m_draggedHoverIndex, m_draggedHoverIndex, {Qt::ItemDataRole::BackgroundRole});
-        m_draggedHoverIndex = QModelIndex();
+  void ClearCopiedDict() {
+    decltype(m_copiedMap) tmp;
+    tmp.swap(m_copiedMap);
+    for (auto it = tmp.cbegin(); it != tmp.cend(); ++it) {
+      for (auto index : it.value()) {
+        emit dataChanged(index, index, {Qt::ItemDataRole::BackgroundRole});
+      }
     }
+  }
 
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override{
-        if (role == Qt::BackgroundRole){
-            if (m_draggedHoverIndex == index){
-                return QBrush(Qt::green);
-            }
-            if (m_cutMap.contains(rootPath()) and m_cutMap[rootPath()].contains(index)){
-                return QBrush(Qt::GlobalColor::yellow);
-            }
-            if (m_copiedMap.contains(rootPath()) and m_copiedMap[rootPath()].contains(index)){
-                return QBrush(Qt::GlobalColor::magenta);
-            }
-            return QBrush(Qt::transparent);
-        }
-        return QFileSystemModel::data(index, role);
+  void CutSomething(const QModelIndexList& cutIndexes, bool appendMode = false) {
+    if (not appendMode) {
+      ClearCutDict();
     }
+    ClearCopiedDict();
+    if (not m_cutMap.contains(rootPath())) {
+      m_cutMap[rootPath()] = {};
+    }
+    m_cutMap[rootPath()] += cutIndexes;
+  }
+  void CopiedSomething(const QModelIndexList& copiedIndexes, bool appendMode = false) {
+    if (not appendMode) {
+      ClearCopiedDict();
+    }
+    ClearCutDict();
+    if (not m_copiedMap.contains(rootPath())) {
+      m_copiedMap[rootPath()] = {};
+    }
+    m_copiedMap[rootPath()] += copiedIndexes;
+  }
 
-public slots:
-    void whenRootPathChanged(const QString& newpath);
-    void whenDirectoryLoaded(const QString& path);
+  void DragHover(const QModelIndex index) {
+    if (m_draggedHoverIndex.row() == index.row() and m_draggedHoverIndex.column() == index.column()) {
+      return;
+    }
+    emit dataChanged(m_draggedHoverIndex, m_draggedHoverIndex, {Qt::ItemDataRole::BackgroundRole});
+    emit dataChanged(index, index, {Qt::ItemDataRole::BackgroundRole});
+    m_draggedHoverIndex = index;
+  }
 
-protected:
-    CustomStatusBar* logger;
-    int m_imagesSizeLoaded = 0;
+  void DragRelease(const QModelIndex index = QModelIndex()) {
+    emit dataChanged(m_draggedHoverIndex, m_draggedHoverIndex, {Qt::ItemDataRole::BackgroundRole});
+    m_draggedHoverIndex = QModelIndex();
+  }
 
-    static int previewsCnt;
-    static constexpr int cacheWidth = 256;
-    static constexpr int cacheHeight = 256;
+  QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override {
+    if (role == Qt::BackgroundRole) {
+      if (m_draggedHoverIndex == index) {
+        return QBrush(Qt::green);
+      }
+      if (m_cutMap.contains(rootPath()) and m_cutMap[rootPath()].contains(index)) {
+        return QBrush(Qt::GlobalColor::yellow);
+      }
+      if (m_copiedMap.contains(rootPath()) and m_copiedMap[rootPath()].contains(index)) {
+        return QBrush(Qt::GlobalColor::magenta);
+      }
+      return QBrush(Qt::transparent);
+    }
+    return QFileSystemModel::data(index, role);
+  }
 
-    static constexpr int IMAGES_COUNT_LOAD_ONCE_MAX = 3;  // 10 pics
-private:
-    QHash<QString, QModelIndexList> m_copiedMap;
-    QHash<QString, QModelIndexList> m_cutMap;
-    QModelIndex m_draggedHoverIndex;
+ public slots:
+  void whenRootPathChanged(const QString& newpath);
+  void whenDirectoryLoaded(const QString& path);
+
+ protected:
+  CustomStatusBar* logger;
+  int m_imagesSizeLoaded = 0;
+
+  static int previewsCnt;
+  static constexpr int cacheWidth = 256;
+  static constexpr int cacheHeight = 256;
+
+  static constexpr int IMAGES_COUNT_LOAD_ONCE_MAX = 3;  // 10 pics
+ private:
+  QHash<QString, QModelIndexList> m_copiedMap;
+  QHash<QString, QModelIndexList> m_cutMap;
+  QModelIndex m_draggedHoverIndex;
 };
 
 #endif  // MYQFILESYSTEMMODEL_H
