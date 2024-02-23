b7ea76655860db9c758c090cd07fd55579bad3e1
Ariel <hancosta@outlook.com>
2024-02-23 09:52:10 -0500

    Version 42.0 Drag/Drop User Experience Enhancement

--- Tools/VideoPlayerWatcher.cpp
+++ Tools/VideoPlayerWatcher.cpp
@@ -1,19 +1,26 @@
 #include "VideoPlayerWatcher.h"
-#include <QMouseEvent>
 #include <QDebug>
+#include <QMouseEvent>
 
 constexpr int VideoPlayerWatcher::RIGHT_EDGE_WIDTH_PIXEL;
 
 bool VideoPlayerWatcher::eventFilter(QObject* watched, QEvent* event) {
-  if (event->type() == QEvent::MouseMove and watched == m_watched and not m_controlled->isVisible()) {
+  if (event->type() == QEvent::MouseMove and watched == m_watched) {
     auto* me = dynamic_cast<QMouseEvent*>(event);
     if (me == nullptr) {
       qWarning("cannot dynamic cast to mouse event");
-      return false;
+      return true;
+    }
+    if (m_controlled->isVisible()) {
+      if (me->pos().x() <= RIGHT_EDGE_WIDTH_PIXEL) {
+        m_controlled->setVisible(false);
+      }
+      return true;
     }
     if (m_watched->width() - me->pos().x() <= RIGHT_EDGE_WIDTH_PIXEL) {
       m_controlled->setVisible(true);
     }
+    return true;
   }
   return QObject::eventFilter(watched, event);
 }
