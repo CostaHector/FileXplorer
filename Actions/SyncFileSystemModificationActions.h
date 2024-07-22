4fb826ee42d0b5764814946270ffb894395dba26
HanCosta <hancosta@outlook.com>
2024-07-22 22:59:37 +0800

    Sync Modification Enhancement

--- Actions/SyncFileSystemModificationActions.h
+++ Actions/SyncFileSystemModificationActions.h
@@ -1,26 +1,26 @@
 #ifndef SYNCFILESYSTEMMODIFICATIONACTIONS_H
 #define SYNCFILESYSTEMMODIFICATIONACTIONS_H
 
-#include <QObject>
 #include <QAction>
 #include <QActionGroup>
 #include <QLineEdit>
+#include <QObject>
 #include <QToolBar>
 
-class SyncFileSystemModificationActions : public QObject
-{
+class SyncFileSystemModificationActions : public QObject {
   Q_OBJECT
  public:
-  explicit SyncFileSystemModificationActions(QObject *parent = nullptr);
+  explicit SyncFileSystemModificationActions(QObject* parent = nullptr);
 
-  QToolBar* GetSyncToolbar();
+  QToolBar* GetSyncSwitchToolbar();
+  QToolBar* GetSyncPathToolbar();
 
   QLineEdit* _BASIC_PATH{nullptr};
   QLineEdit* _SYNC_TO_PATH{nullptr};
-  QAction* _SYNC_MOD_SWITCH{new QAction("Sync switch", this)};
-  QAction* _SYNC_REVERSE_SWITCH{new QAction("Sync reverse back switch", this)};
+  QAction* _SYNC_MOD_SWITCH{new QAction(QIcon(":/themes/SYNC_MODIFICATION_SWITCH"), "sync switch", this)};
+  QAction* _SYNC_REVERSE_SWITCH{new QAction(QIcon(":/themes/SYNC_REVERSE_SWITCH"), "reverse back", this)};
 };
 
 SyncFileSystemModificationActions& g_syncFileSystemModificationActions();
 
-#endif // SYNCFILESYSTEMMODIFICATIONACTIONS_H
+#endif  // SYNCFILESYSTEMMODIFICATIONACTIONS_H
