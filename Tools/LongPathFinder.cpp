09cc4a8915daa99991f71424192715c81aa9abbd
HanCosta <hancosta@outlook.com>
2024-09-20 19:01:28 +0800

    copy full records shortcuts

--- Tools/LongPathFinder.cpp
+++ Tools/LongPathFinder.cpp
@@ -3,7 +3,7 @@
 #include <QDirIterator>
 #include <QFileInfo>
 
-int LongPathFinder::MAX_PATH_LENGTH{260};
+constexpr int LongPathFinder::MAX_PATH_LENGTH;
 
 void LongPathFinder::SetDropSectionWhenTooLong(int section) {
   dropSection = section;
@@ -15,27 +15,19 @@ void LongPathFinder::SetDropSectionWhenTooLong(int section) {
     return;
   }
   news.clear();
-  for (const QString& oldFolderName: olds) {
+  for (const QString& oldFolderName : olds) {
     const QString& newFolderName = GetNewFolderName(oldFolderName);
     news.append(newFolderName);
   }
 }
 
-QString LongPathFinder::GetNewFolderName(const QString& oldName) const{
+QString LongPathFinder::GetNewFolderName(const QString& oldName) const {
   auto oldNameSectionsList = oldName.split(" - ");
   if (oldNameSectionsList.size() <= 1 || oldNameSectionsList.size() < std::abs(dropSection)) {
     return oldName;
   }
   oldNameSectionsList.erase(oldNameSectionsList.end() + dropSection);
   return oldNameSectionsList.join(" - ");
-}
-
-// folderNameLen = N - backSlashInd - 1
-// N + 1 + 4 + N - backSlashInd - 1 = 2N - backSlashInd + 4
-bool LongPathFinder::LengthBelow(const QString& path) {
-  const int backSlashInd = path.lastIndexOf('/');
-  const int N = path.size();
-  return 2 * N - backSlashInd + 4 < MAX_PATH_LENGTH;
 }
 
 int LongPathFinder::operator()(const QString& path) {
@@ -55,7 +47,7 @@ int LongPathFinder::operator()(const QString& path) {
   QDirIterator it(path, {}, QDir::Filter::Dirs | QDir::Filter::NoDotAndDotDot, QDirIterator::IteratorFlag::Subdirectories);
   while (it.hasNext()) {
     const QString& currentPath = it.next();
-    if (LengthBelow(currentPath)) {
+    if (IsLengthBelowSysLimit(currentPath)) {
       continue;
     }
     const QString& oldFolderName = QFileInfo(currentPath).fileName();
@@ -69,16 +61,16 @@ int LongPathFinder::operator()(const QString& path) {
   return overlongCnt;
 }
 
-int LongPathFinder::Check() {
+int LongPathFinder::CheckTooLongPathCount() {
   m_status.clear();
   if (pres.size() != olds.size() || olds.size() != news.size()) {
     qWarning("length not match[%d, %d, %d]", pres.size(), olds.size(), news.size());
     return 0;
   }
   m_status.reserve(pres.size());
-  // len(pre) + 1 + len(newName) + 1 + len(newName) + 4 < MAX_PATH_LENGTH
+  // len(pre) + 1 + len(newName) + 1 + len(oldName) + 5 < MAX_PATH_LENGTH
   for (int i = 0; i < pres.size(); ++i) {
-    if (pres[i].size() + 2 * news[i].size() + 6 < MAX_PATH_LENGTH) {
+    if (pres[i].size() + news[i].size() + olds[i].size() + 7 < MAX_PATH_LENGTH) {
       m_status += '\n';
       continue;
     }
