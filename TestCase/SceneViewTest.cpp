6b51c9dad66c1beaf940441ddb5452340589b3ef
HanCosta <hancosta@outlook.com>
2024-10-01 01:01:02 +0800

    Enhance Scene Read Json Speed by plaint text scn files

--- TestCase/SceneViewTest.cpp
+++ TestCase/SceneViewTest.cpp
@@ -2,6 +2,7 @@
 #include <QtTest>
 
 // #include "pub/BeginToExposePrivateMember.h"
+#include "Tools/PathTool.h"
 #include "Tools/SceneInfoManager.h"
 // #include "pub/EndToExposePrivateMember.h"
 #include "PublicVariable.h"
@@ -13,6 +14,8 @@
 
 const QString SCENE_VIEW_PATH{QDir(QFileInfo(__FILE__).absolutePath()).absoluteFilePath("test/TestEnv_SceneViewTest/DONT_CHANGE")};
 const QString TEST_DIR{QDir(QFileInfo(__FILE__).absolutePath()).absoluteFilePath("test/TestEnv_SceneViewTest/COPY_REMOVABLE")};
+
+using namespace SceneInfoManager;
 
 class SceneViewTest : public QObject {
   Q_OBJECT
@@ -49,62 +52,63 @@ class SceneViewTest : public QObject {
   }
 
   void test_nonExistfolder_ok() {
-    SceneInfoManager sim;
-    auto scenes = sim.GetScenesFromPath("any/not/exist/path");
+    auto scenes = SceneInfoManager::GetScenesFromPath("any/not/exist/path");
     QVERIFY(scenes.isEmpty());
   }
 
   void test_3jsonsFolder_ok() {
-    SceneInfoManager sim;
-    auto scenes = sim.GetScenesFromPath(TEST_DIR);
+    QCOMPARE(SceneInfoManager::GenerateScnFiles(TEST_DIR), 1);
+
+    auto scenes = SceneInfoManager::GetScenesFromPath(TEST_DIR);
     QCOMPARE(scenes.size(), 3);
 
-    sim.swap(scenes);
-    QVERIFY(scenes.isEmpty());
+    QCOMPARE(scenes[0].name, "somejsonfile1");
+    QCOMPARE(scenes[1].name, "somejsonfile2");
+    QCOMPARE(scenes[2].name, "somejsonfileEmpty");
+    SceneInfoManager::sort(scenes, SortByKey::NAME, true);
+    QCOMPARE(scenes[0].name, "somejsonfileEmpty");
+    QCOMPARE(scenes[1].name, "somejsonfile2");
+    QCOMPARE(scenes[2].name, "somejsonfile1");
+    SceneInfoManager::sort(scenes, SortByKey::NAME, false);
+    QCOMPARE(scenes[0].name, "somejsonfile1");
+    QCOMPARE(scenes[1].name, "somejsonfile2");
+    QCOMPARE(scenes[2].name, "somejsonfileEmpty");
 
-    QCOMPARE(sim[0].name, "somejsonfile1");
-    QCOMPARE(sim[1].name, "somejsonfile2");
-    QCOMPARE(sim[2].name, "somejsonfileEmpty");
-    sim.sort(SceneInfoManager::NAME, true);
-    QCOMPARE(sim[0].name, "somejsonfileEmpty");
-    QCOMPARE(sim[1].name, "somejsonfile2");
-    QCOMPARE(sim[2].name, "somejsonfile1");
-    sim.sort(SceneInfoManager::NAME, false);
-    QCOMPARE(sim[0].name, "somejsonfile1");
-    QCOMPARE(sim[1].name, "somejsonfile2");
-    QCOMPARE(sim[2].name, "somejsonfileEmpty");
-
-    sim.sort(SceneInfoManager::RATE, true);
-    QCOMPARE(sim[0].rate, 99);
-    QCOMPARE(sim[1].rate, 50);
-    QCOMPARE(sim[2].rate, 0);
-    sim.sort(SceneInfoManager::RATE, false);
-    QCOMPARE(sim[0].rate, 0);
-    QCOMPARE(sim[1].rate, 50);
-    QCOMPARE(sim[2].rate, 99);
+    SceneInfoManager::sort(scenes, SortByKey::RATE, true);
+    QCOMPARE(scenes[0].rate, 99);
+    QCOMPARE(scenes[1].rate, 50);
+    QCOMPARE(scenes[2].rate, 0);
+    SceneInfoManager::sort(scenes, SortByKey::RATE, false);
+    QCOMPARE(scenes[0].rate, 0);
+    QCOMPARE(scenes[1].rate, 50);
+    QCOMPARE(scenes[2].rate, 99);
   }
-  void test_UpdateInExistPath() { QCOMPARE(SceneInfoManager::UpdateJsonImgVidSize("Any/Inexist/Path"), -1); }
+  void test_UpdateInExistPath() {
+    QVERIFY(!QFileInfo("Any/Inexist/Path").isDir());
+    QCOMPARE(SceneInfoManager::UpdateJsonImgVidSize("Any/Inexist/Path"), -1);
+  }
 
   void test_UpdateJsonVidImgRateUploaded() {
-    SceneInfoManager sim;
-    auto beforeScenes = sim.GetScenesFromPath(TEST_DIR);
-    QCOMPARE(beforeScenes[2].name, "somejsonfileEmpty");
-    QCOMPARE(beforeScenes[2].imgName, "");
-    QCOMPARE(beforeScenes[2].vidName, "");
-    QCOMPARE(beforeScenes[2].vidSize, 0);
-    QCOMPARE(beforeScenes[2].rate, 0);
-    QCOMPARE(beforeScenes[2].uploaded, "");
-
     QCOMPARE(SceneInfoManager::UpdateJsonImgVidSize(TEST_DIR), 1);
-    auto afterScenes = sim.GetScenesFromPath(TEST_DIR);
+
+    QVERIFY(!QDir(TEST_DIR).exists("COPY_REMOVABLE.scn"));
+    QCOMPARE(SceneInfoManager::GenerateScnFiles(TEST_DIR), 1);
+
+    auto afterScenes = SceneInfoManager::GetScenesFromPath(TEST_DIR);
     QCOMPARE(afterScenes[2].name, "somejsonfileEmpty");
     QCOMPARE(afterScenes[2].imgName, "somejsonfileEmpty.png");
     QCOMPARE(afterScenes[2].vidName, "somejsonfileEmpty.mp4");
     QVERIFY(afterScenes[2].vidSize > 0);
     QCOMPARE(afterScenes[2].rate, 0);
     QCOMPARE(afterScenes[2].uploaded.size(), QString("yyyyMMdd hh:mm:ss").size());
+  }
 
-    QCOMPARE(SceneInfoManager::UpdateJsonImgVidSize(TEST_DIR), 0);
+  void test_WriteScenesIntoScnFile() {
+    const QString scnFileName = PATHTOOL::fileName(TEST_DIR) + ".scn";
+    QVERIFY2(!QDir(TEST_DIR).exists(scnFileName), "*.scn file should not exists yet");
+    QCOMPARE(SceneInfoManager::GenerateScnFiles(TEST_DIR), 1);
+    QVERIFY2(QDir(TEST_DIR).exists(scnFileName), "*.scn file should exists");
+    QCOMPARE(SceneInfoManager::GenerateScnFiles(TEST_DIR), 1);
   }
 };
 
--- Tools/SceneInfoManager.h
+++ Tools/SceneInfoManager.h
@@ -5,6 +5,7 @@
 #include <QList>
 
 struct SCENE_INFO {
+  QString rel2scn;  // jsonFullPath = mRootPath + rel2scn + jsonFileName, rel2scn can be '/' or '/any thing/'
   QString name;      // name, key"Name"
   QString imgName;   // img, key"ImgName"
   QString vidName;   // video, key"VidName"
@@ -15,21 +16,16 @@ struct SCENE_INFO {
 
 typedef QList<SCENE_INFO> SCENES_TYPE;
 
-class SceneInfoManager {
- public:
-  static int UpdateJsonImgVidSize(const QString& path);
-  static SCENES_TYPE GetScenesFromPath(const QString& path, const bool enableFilter=false, const QString& pattern = {}, SCENES_TYPE* pFiltered = nullptr);
+namespace SceneInfoManager {
+typedef enum class tagSortByKey { NAME = 0, SIZE, RATE, UPLOADED, BUTT } SortByKey;
 
-  typedef enum tagSortByKey { NAME = 0, SIZE, RATE, UPLOADED, SORT_BY_KEY_BUTT } SortByKey;
-  void sort(const SortByKey sortByKey = NAME, const bool reverse = false);
+int UpdateJsonImgVidSize(const QString& path);
+SCENES_TYPE ScnFileParser(const QString& scnFileFullPath, const QString rel,
+                          bool enableFilter, const QString& pattern, SCENES_TYPE* pFilterd = nullptr);
+int GenerateScnFiles(const QString& path);
 
-  void swap(SCENES_TYPE& rhs) { mScenes.swap(rhs); }
-  inline int size() { return mScenes.size(); }
-  inline int isEmpty() { return mScenes.isEmpty(); }
-  const SCENE_INFO& operator[](const int index) const { return mScenes[index]; }
-
- private:
-  SCENES_TYPE mScenes;
-};
+SCENES_TYPE GetScenesFromPath(const QString& path, const bool enableFilter = false, const QString& pattern = {}, SCENES_TYPE* pFiltered = nullptr);
+SCENES_TYPE& sort(SCENES_TYPE& scenes, SortByKey sortByKey = tagSortByKey::NAME, const bool reverse = false);
+}  // namespace SceneInfoManager
 
 #endif  // SCENEINFOMANAGER_H
