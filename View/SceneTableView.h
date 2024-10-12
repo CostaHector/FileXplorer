ce76f991714bd2bb127f8a75fa1ce1f324387714
HanCosta <hancosta@outlook.com>
2024-10-12 22:03:13 +0800

    Version 50.4 Message Log output fixed/flush time enhance

--- View/SceneTableView.h
+++ View/SceneTableView.h
@@ -1,17 +1,20 @@
 #ifndef SCENETABLEVIEW_H
 #define SCENETABLEVIEW_H
 
-
 #include "View/CustomTableView.h"
 class ScenesTableModel;
 class QStyledItemDelegate;
 class SceneActionsSubscribe;
+class QMenu;
 class SceneTableView : public CustomTableView {
  public:
   explicit SceneTableView(ScenesTableModel* sceneModel, QWidget* parent = nullptr);
   void setRootPath(const QString& rootPath);
-
+  void subscribe();
+  void onCopyBaseName();
  private:
+  QAction* COPY_BASENAME_FROM_SCENE{nullptr};
+  QMenu* m_menu{nullptr};
   ScenesTableModel* _sceneModel;
   QStyledItemDelegate* mAlignDelegate{nullptr};
 };
