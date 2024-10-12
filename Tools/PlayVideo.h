000392e2e4fa2876b84c6d5caf2d0cd73cf47ca3
HanCosta <hancosta@outlook.com>
2024-10-12 22:51:18 +0800

    Scene Table View Play this folder

--- Tools/PlayVideo.h
+++ Tools/PlayVideo.h
@@ -1,23 +1,10 @@
 #ifndef PLAYVIDEO_H
 #define PLAYVIDEO_H
 
-class PlayVideo {
- public:
-  PlayVideo();
-};
 
-#include <QDesktopServices>
-#include <QDir>
-#include <QFile>
-#include <QFileInfo>
-#include <QProcess>
 #include <QString>
-#include <QUrl>
-
-#include "PublicVariable.h"
-
 bool PlayADir(const QString& dirPath);
 
-bool on_ShiftEnterPlayVideo(QString& path);
+bool on_ShiftEnterPlayVideo(const QString& path);
 
 #endif  // PLAYVIDEO_H
