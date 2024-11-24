44d17f748f3921d1cddb415bdb3f9c48b7161321
HanCosta <hancosta@outlook.com>
2024-11-24 10:17:50 +0800

    Version 60.2.0 Thumbnail custom range image index supported

--- Actions/ThumbnailProcessActions.cpp
+++ Actions/ThumbnailProcessActions.cpp
@@ -1,10 +1,15 @@
 #include "ThumbnailProcessActions.h"
 
 ThumbnailProcessActions::ThumbnailProcessActions(QObject* parent) {
-  _EXTRACT_1ST_IMG = new QAction(QIcon(":img/THUMBNAIL_EXTRACTOR"), "1st img");
-  _EXTRACT_2ND_IMGS = new QAction("first 2 imgs");
-  _EXTRACT_4TH_IMGS = new QAction("first 4 imgs");
-  _CUSTOM_RANGE_IMGS = new QAction("custom range imgs [b, e)");
+  _EXTRACT_1ST_IMG = new QAction(QIcon(":img/THUMBNAIL_EXTRACTOR"), "Thumbnail Extractor");
+  _EXTRACT_1ST_IMG->setToolTip("Extract [0, 1) image from thumbnails under current view path");
+  _EXTRACT_2ND_IMGS = new QAction("Extract [0, 2) imgs");
+  _EXTRACT_2ND_IMGS->setToolTip("Extract [0, 2) images from thumbnails under current view path");
+  _EXTRACT_4TH_IMGS = new QAction("Extract [0, 4) imgs");
+  _EXTRACT_4TH_IMGS->setToolTip("Extract [0, 4) images from thumbnails under current view path");
+  _CUSTOM_RANGE_IMGS = new QAction("Extract [b, e) imgs");
+  _CUSTOM_RANGE_IMGS->setToolTip("Extract [b, e) images from thumbnails under current view path");
+
   _SKIP_IF_ALREADY_EXIST = new QAction(QIcon(""), "Skip extract if already exist");
   _SKIP_IF_ALREADY_EXIST->setCheckable(true);
   _SKIP_IF_ALREADY_EXIST->setChecked(true);
