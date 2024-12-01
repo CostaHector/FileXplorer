60353843a477551c42fd8b597d90c218cfc551f0
HanCosta <hancosta@outlook.com>
2024-12-01 11:18:25 +0800

    Log Handler Enhancement

--- Tools/LogHandler.h
+++ Tools/LogHandler.h
@@ -1,10 +1,8 @@
 #ifndef LOGHANDLER_H
 #define LOGHANDLER_H
 #include <QFile>
-#include <QMessageLogContext>
-
 #include <QTextStream>
-#include <QTime>
+
 class LogHandler : QObject {
  public:
   explicit LogHandler(QObject* parent = nullptr);
@@ -14,10 +12,10 @@ class LogHandler : QObject {
 #ifdef QT_DEBUG
     fflush(stdout);
 #else
-    if (ts.device() == nullptr || !outFile.isOpen()) {
+    if (mLogTextStream.device() == nullptr || !mLogFile.isOpen()) {
       return;
     }
-    ts.flush();
+    mLogTextStream.flush();
 #endif
   }
   static bool subscribe();
@@ -32,10 +30,11 @@ class LogHandler : QObject {
 
   static const QString LOG_ABSFILENAME_TEMPLATE;
   static QtMsgType OUTPUT_LOG_LEVEL;
-  static bool m_flushInstantly;
-  static QString logPrePath;
-  static QFile outFile;
-  static QTextStream ts;
+  static bool mFlushLogInBufferInstantly;
+  static QString mLogFolderPath;
+
+  static QFile mLogFile;
+  static QTextStream mLogTextStream;
 };
 
 #endif  // LOGHANDLER_H
