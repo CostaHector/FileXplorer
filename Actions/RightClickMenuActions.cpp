9f73c0e0e2fea0cce6bf0b2551d7a3529756c4a3
ariel <costahan@gmail.com>
2024-12-04 22:37:43 +0800

    no watch change when pack/unpack piles of files

--- Actions/RightClickMenuActions.cpp
+++ Actions/RightClickMenuActions.cpp
@@ -1,15 +1,32 @@
 #include "RightClickMenuActions.h"
 
 RightClickMenuActions::RightClickMenuActions(QObject* parent)
-    : QObject{parent},
-      _CALC_MD5_ACT(new QAction(QIcon(":img/MD5_FILE_IDENTIFIER_PATH"), tr("MD5 (File Indentifier)"))),
-      _PROPERTIES(new QAction(QIcon(":img/PROPERTIES"), tr("Properties")))
+  : QObject{parent} {
+  _CALC_MD5_ACT = new (std::nothrow) QAction(QIcon(":img/MD5_FILE_IDENTIFIER_PATH"), tr("MD5 (File Indentifier)"));
+  if (_CALC_MD5_ACT == nullptr) {
+    qCritical("_CALC_MD5_ACT is nullptr");
+    return;
+  }
+  _PROPERTIES = new (std::nothrow) QAction(QIcon(":img/PROPERTIES"), tr("Properties"));
+  if (_PROPERTIES == nullptr) {
+    qCritical("_PROPERTIES is nullptr");
+    return;
+  }
+  _FORCE_REFRESH_FILESYSTEMMODEL = new (std::nothrow) QAction{"Refresh"};
+  if (_FORCE_REFRESH_FILESYSTEMMODEL == nullptr) {
+    qCritical("_FORCE_REFRESH_FILESYSTEMMODEL is nullptr");
+    return;
+  }
 
-{
   _PROPERTIES->setShortcut(QKeySequence(Qt::KeyboardModifier::AltModifier | Qt::Key::Key_Return));
   _PROPERTIES->setShortcutVisibleInContextMenu(true);
   _PROPERTIES->setToolTip(
-      QString("<b>%1 (%2)</b><br/>Show the properties fot the selected item(s)").arg(_PROPERTIES->text(), _PROPERTIES->shortcut().toString()));
+        QString("<b>%1 (%2)</b><br/>Show the properties for the selected item(s)").arg(_PROPERTIES->text(), _PROPERTIES->shortcut().toString()));
+
+  _FORCE_REFRESH_FILESYSTEMMODEL->setShortcut(QKeySequence(Qt::Key::Key_F5));
+  _FORCE_REFRESH_FILESYSTEMMODEL->setShortcutVisibleInContextMenu(true);
+  _FORCE_REFRESH_FILESYSTEMMODEL->setToolTip(
+        QString("<b>%1 (%2)</b><br/>Force refresh file system mode after item change.").arg(_FORCE_REFRESH_FILESYSTEMMODEL->text(), _FORCE_REFRESH_FILESYSTEMMODEL->shortcut().toString()));
 }
 
 RightClickMenuActions& g_rightClickActions()
