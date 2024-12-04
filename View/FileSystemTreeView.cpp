d06a9e71591033c088ea85482025c647a6248185
ariel <costahan@gmail.com>
2024-12-04 22:37:43 +0800

    no watch change when pack/unpack piles of files

--- View/FileSystemTreeView.cpp
+++ View/FileSystemTreeView.cpp
@@ -31,6 +31,7 @@ void FileSystemTreeView::subscribe() {
 
   addAction(g_rightClickActions()._CALC_MD5_ACT);
   addAction(g_rightClickActions()._PROPERTIES);
+  addAction(g_rightClickActions()._FORCE_REFRESH_FILESYSTEMMODEL);
 
   addActions(g_viewActions()._VIEW_ACRIONS->actions());
   addActions(g_fileBasicOperationsActions().OPEN_AG->actions());
