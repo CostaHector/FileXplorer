b06f531f035cd7996eb3926b24ed66f4733f51a3
ariel <costahan@gmail.com>
2024-12-15 20:13:39 +0800

    Version 60.3.9 DropdownToolButton Enhancement

--- Component/DropListToolButton.h
+++ Component/DropListToolButton.h
@@ -9,9 +9,13 @@ class QActionGroup;
 QToolButton* DropListToolButton(QAction* defaultAction,
                                 QList<QAction*> dropdownActions,
                                 QToolButton::ToolButtonPopupMode popupMode = QToolButton::ToolButtonPopupMode::InstantPopup,
-                                const QString& updateToolTip = "",
                                 const Qt::ToolButtonStyle toolButtonStyle = Qt::ToolButtonStyle::ToolButtonTextUnderIcon,
                                 const int iconSize = TABS_ICON_IN_MENU_1x1);
-
+QToolButton* DropListToolButtonWithoutDefAction(const QIcon &icon, const QString &text, const QString& toolTip,
+                                QList<QAction*> dropdownActions,
+                                QToolButton::ToolButtonPopupMode popupMode = QToolButton::ToolButtonPopupMode::InstantPopup,
+                                const Qt::ToolButtonStyle toolButtonStyle = Qt::ToolButtonStyle::ToolButtonTextUnderIcon,
+                                const int iconSize = TABS_ICON_IN_MENU_1x1);
 auto FindQActionFromQActionGroupByActionName(const QString& actionName, QActionGroup* ag) -> QAction*;
+bool EnableAutoChangeDefaultActionInToolButton(QToolButton* pTb, const QString& MEMORY_KEY);
 #endif // DROPLISTTOOLBUTTON_H
