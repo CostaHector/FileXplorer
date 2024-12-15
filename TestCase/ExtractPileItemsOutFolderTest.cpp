91c2f6cda5d9fbb85195e73bf6ba0ac5ef208221
ariel <costahan@gmail.com>
2024-12-15 22:07:34 +0800

    Fix ScenesMixed img file endwith number

--- TestCase/ExtractPileItemsOutFolderTest.cpp
+++ TestCase/ExtractPileItemsOutFolderTest.cpp
@@ -86,7 +86,20 @@ class ExtractPileItemsOutFolderTest : public QObject {
     QCOMPARE(sMixed.m_vid2Name["another name"].size(), 2);
     QCOMPARE(sMixed.m_json2Name["another name"], "another name.json");
   }
+
+  void test_MixScenesEndWithNumber() {
+    QStringList files;
+    files << "name98.mp4"
+          << "name98.jpg"
+          << "name98 1.jpg"
+          << "name98.json";
+    ScenesMixed sMixed;
+    QCOMPARE(sMixed(files), 1); // 1 json file, 1
+    QCOMPARE(sMixed.m_img2Name["name98"].size(), 2);
+    QCOMPARE(sMixed.m_img2Name["name98"][0], "name98.jpg");
+    QCOMPARE(sMixed.m_img2Name["name98"][1], "name98 1.jpg");
+  }
 };
 
-//QTEST_MAIN(ExtractPileItemsOutFolderTest)
+QTEST_MAIN(ExtractPileItemsOutFolderTest)
 #include "ExtractPileItemsOutFolderTest.moc"
