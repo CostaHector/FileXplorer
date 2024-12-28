aab70f03a79039e80c1d7e54cf2929fdf51cc536
HanCosta <hancosta@outlook.com>
2024-12-28 21:13:36 +0800

    Version 60.4.0 Search mode in memory

--- Component/SearchModeComboBox.cpp
+++ Component/SearchModeComboBox.cpp
@@ -4,6 +4,8 @@
 SearchModeComboBox::SearchModeComboBox(QWidget* parent) : QComboBox{parent} {
   addItems({"Normal", "Wildcard", "Regex", "Search for File Content"});
   setSizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
+
+  setCurrentText(PreferenceSettings().value("ADVANCE_SEARCH_MODE", "Wildcard").toString());
 }
 
 void SearchModeComboBox::BindSearchModel(SearchProxyModel* searchProxyModel) {
