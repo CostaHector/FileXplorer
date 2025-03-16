0cd89a38a11f5509f53cdb6d6a836b6f59a957c7
HanCosta <hancosta@outlook.com>
2025-03-16 17:46:43 +0800

    Enhance Json Image

---
+++ TestCase/JsonEditorToolTest.cpp
@@ -0,0 +1,27 @@
+#include <QtTest>
+#include <QCoreApplication>
+#include "pub/MyTestSuite.h"
+
+
+
+class JsonEditorToolTest : public MyTestSuite
+{
+  Q_OBJECT
+ public:
+
+ private slots:
+  void initTestCase() {
+    qDebug("Start to test JsonEditorToolTest ...");
+  }
+  void cleanupTestCase() {
+    qDebug("End to test JsonEditorToolTest ...");
+  }
+  void test_save() {
+  }
+  void test_cancel() {
+  }
+};
+
+JsonEditorToolTest g_jsonEditorToolTest;
+
+#include "JsonEditorToolTest.moc"
