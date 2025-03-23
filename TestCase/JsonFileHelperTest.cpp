bab50691d6a4e7ba43f819131f6e83b05848981d
HanCosta <hancosta@outlook.com>
2025-03-23 23:48:45 +0800

    VariantHashHelper Callable Object Testcase

--- TestCase/JsonFileHelperTest.cpp
+++ TestCase/JsonFileHelperTest.cpp
@@ -52,12 +52,144 @@ class JsonFileHelperTest : public MyTestSuite {
     QCOMPARE(dict["Chris Evans"].toInt(), 192);
   }
 
-  void test_jsonStr2DictCompatible() {
+  void test_CompatibleJsonKey() {
     const char jsonStrArray[] = R"({"ProductionStudio": ["Chris Hemsworth Studio", "Keanu Reeves Studio"]})";
     const QStringList expectSl{"Chris Hemsworth Studio", "Keanu Reeves Studio"};
     const QVariantHash dict = JsonFileHelper::JsonStr2Dict(jsonStrArray);
     QVERIFY(dict.contains(JSONKey::Studio));
     QCOMPARE(dict[JSONKey::Studio].toStringList(), expectSl);
+  }
+
+  void test_ClearPerformerAndStudio_ok() {
+    const char jsonStrArray[] = R"({"Performers": ["Chris Hemsworth Studio", "Keanu Reeves Studio"], "Name": "Marvel Film"})";
+    QVariantHash dictWithPerfAndStudio = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    QVERIFY(dictWithPerfAndStudio.contains(JSONKey::Performers));
+    QVERIFY(!dictWithPerfAndStudio.contains(JSONKey::Studio));
+    QVERIFY(!dictWithPerfAndStudio[JSONKey::Performers].toStringList().isEmpty());
+    VariantHashHelper::ClearPerformerAndStudio cps;
+    QVERIFY(cps(dictWithPerfAndStudio));  // has cleared
+    QVERIFY(dictWithPerfAndStudio.contains(JSONKey::Performers));
+    QVERIFY(!dictWithPerfAndStudio.contains(JSONKey::Studio));
+    QVERIFY(dictWithPerfAndStudio[JSONKey::Performers].toStringList().isEmpty());
+  }
+
+  void test_ClearPerformerAndStudio_skip_keyNotExist() {
+    const char jsonStrArray[] = R"({"Name": "Chris Hemsworth life story"})";
+    QVariantHash dictWithOnlyName = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    QVERIFY(dictWithOnlyName.contains(JSONKey::Name));
+    QVERIFY(!dictWithOnlyName.contains(JSONKey::Performers));
+    QVERIFY(!dictWithOnlyName.contains(JSONKey::Studio));
+    VariantHashHelper::ClearPerformerAndStudio cps;
+    QVERIFY(!cps(dictWithOnlyName));  // has cleared
+    QVERIFY(dictWithOnlyName.contains(JSONKey::Name));
+    QVERIFY(!dictWithOnlyName.contains(JSONKey::Performers));
+    QVERIFY(!dictWithOnlyName.contains(JSONKey::Studio));
+  }
+
+  void test_InsertPerfsPairToDictByNameHint_skip_perfsPair_alreadyExist() {
+    const char jsonStrArray[] = R"({"Performers": ["Chris Hemsworth Studio", "Keanu Reeves Studio"]})";
+    const QStringList& expectSl{"Chris Hemsworth Studio", "Keanu Reeves Studio"};
+    QVariantHash dictWithPerfs = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    VariantHashHelper::InsertPerfsPairToDictByNameHint ipdnh;
+    QVERIFY(!ipdnh(dictWithPerfs));
+    auto perfIt = dictWithPerfs.find(JSONKey::Performers);
+    QVERIFY(perfIt != dictWithPerfs.end());
+    QCOMPARE(perfIt.value().toStringList(), expectSl);
+  }
+
+  void test_InsertPerfsPairToDictByNameHint_perfsPair_byHint_insertOk() {
+    const char jsonStrArray[] = R"({"Name":""})";
+    const QStringList expectEmptySl;
+    QVariantHash dictWithPerfs = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    VariantHashHelper::InsertPerfsPairToDictByNameHint ipdnh;
+    QVERIFY(ipdnh(dictWithPerfs));
+    auto perfIt = dictWithPerfs.find(JSONKey::Performers);
+    QVERIFY(perfIt != dictWithPerfs.end());
+    QCOMPARE(perfIt.value().toStringList(), expectEmptySl);
+  }
+
+  void test_AppendPerfsToDict_sorted_and_unique_ok() {
+    const QString toBeInsertedPerfs{"A, B, and C, D"};
+    VariantHashHelper::AppendPerfsToDict apd{toBeInsertedPerfs};
+    const char jsonStrArray[] = R"({"Performers":["A", "E"]})";
+    QVariantHash dictPerfs = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    const QStringList alreadyExistPerfs{"A", "E"};
+    QCOMPARE(dictPerfs[JSONKey::Performers], alreadyExistPerfs);
+    QVERIFY(apd(dictPerfs));  // B C D inserted ok, A E already exist
+    auto perfIt = dictPerfs.find(JSONKey::Performers);
+    QVERIFY(perfIt != dictPerfs.end());
+    const QStringList afterInsertedPerfs{"A", "B", "C", "D", "E"};
+    QCOMPARE(perfIt.value().toStringList(), afterInsertedPerfs);
+  }
+
+  void test_AppendPerfsToDict_skip_alreadyExist() {
+    const QString toBeInsertedPerfs{"A and E"};
+    VariantHashHelper::AppendPerfsToDict apd{toBeInsertedPerfs};
+    const char jsonStrArray[] = R"({"Performers":["A", "E"]})";
+    QVariantHash dictPerfs = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    const QStringList alreadyExistPerfs{"A", "E"};
+    QCOMPARE(dictPerfs[JSONKey::Performers], alreadyExistPerfs);
+    QVERIFY(!apd(dictPerfs));  // A E already exist, nothing inserted
+    auto perfIt = dictPerfs.find(JSONKey::Performers);
+    QVERIFY(perfIt != dictPerfs.end());
+    QCOMPARE(perfIt.value().toStringList(), alreadyExistPerfs);
+  }
+
+  void test_InsertStudioPairIntoDict_skip_studioPair_alreadyExist() {
+    VariantHashHelper::InsertStudioPairIntoDict ispd;
+    const char jsonStrArray[] = R"({"Studio":"Marvel Films"})";
+    QVariantHash dict = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    QVERIFY(dict.contains(JSONKey::Studio));
+    QCOMPARE(dict[JSONKey::Studio].toString(), "Marvel Films");
+    QVERIFY(!ispd(dict));
+    auto studioIt = dict.find(JSONKey::Studio);
+    QVERIFY(studioIt != dict.end());
+    QCOMPARE(studioIt.value().toString(), "Marvel Films");
+  }
+
+  void test_InsertStudioPairIntoDict_studioPair_byHint_insertOk() {
+    VariantHashHelper::InsertStudioPairIntoDict ispd;
+    const char jsonStrArray[] = R"({"Name":""})";
+    QVariantHash dict = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    QVERIFY(!dict.contains(JSONKey::Studio));
+    QVERIFY(ispd(dict));
+    auto studioIt = dict.find(JSONKey::Studio);
+    QVERIFY(studioIt != dict.end());
+    QCOMPARE(studioIt.value().toString(), "");
+  }
+
+  void test_UpdateStudio_skip_studio_notChange() {
+    VariantHashHelper::UpdateStudio us{"Fox"};
+    const char jsonStrArray[] = R"({"Name":"", "Studio": "Fox"})";
+    QVariantHash dict = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    QVERIFY(dict.contains(JSONKey::Studio));
+    QCOMPARE(dict[JSONKey::Studio].toString(), "Fox");
+    QVERIFY(!us(dict));
+    QVERIFY(dict.contains(JSONKey::Studio));
+    QCOMPARE(dict[JSONKey::Studio].toString(), "Fox");
+  }
+
+  void test_UpdateStudio_studio_pair_insertOk() {
+    VariantHashHelper::UpdateStudio us{"Fox"};
+    const char jsonStrArray[] = R"({"Name":""})";
+    QVariantHash dict = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    QVERIFY(!dict.contains(JSONKey::Studio));
+    QVERIFY(us(dict));
+    auto studioIt = dict.find(JSONKey::Studio);
+    QVERIFY(studioIt != dict.end());
+    QCOMPARE(studioIt.value().toString(), "Fox");
+  }
+
+  void test_UpdateStudio_studioName_ChangedOk() {
+    VariantHashHelper::UpdateStudio us{"Fox"};
+    const char jsonStrArray[] = R"({"Name":"", "Studio": "Marvel"})";
+    QVariantHash dict = JsonFileHelper::JsonStr2Dict(jsonStrArray);
+    QVERIFY(dict.contains(JSONKey::Studio));
+    QCOMPARE(dict[JSONKey::Studio].toString(), "Marvel");
+    QVERIFY(us(dict));
+    auto studioIt = dict.find(JSONKey::Studio);
+    QVERIFY(studioIt != dict.end());
+    QCOMPARE(studioIt.value().toString(), "Fox");
   }
 
   void test_GetJsonFilePath() {
--- Tools/NameTool.h
+++ Tools/NameTool.h
@@ -6,62 +6,12 @@
 #include <QRegularExpressionMatchIterator>
 class NameTool {
  public:
-  QStringList operator()(const QString& s) const {
-    QStringList ans;
-    foreach (QString nm, s.split(NameTool::FS_COMP)) {
-      const QString& stdName = nm.trimmed();
-      if (not stdName.isEmpty()) {
-        ans.append(stdName);
-      }
-    }
-    ans.removeDuplicates();
-    return ans;
-  }
-  QStringList fromArticleCapitalizedNames(const QString& s) const {
-    QRegularExpressionMatchIterator it = NAME_COMP.globalMatch(s);
-    QStringList ans;
-    while (it.hasNext()) {
-      QRegularExpressionMatch match = it.next();
-      if (match.hasMatch()) {
-        const QString& caurseName = match.captured(0);
-        const QString& stdName = CapitaliseEachWordFirstLetterLowercaseOthers(caurseName);
-        ans.append(stdName);
-      }
-    }
-    ans.removeDuplicates();
-    return ans;
-  }
+  QStringList operator()(const QString& s) const;
+  QStringList fromArticleCapitalizedNames(const QString& s) const;
 
-  static QString CapitaliseEachWordFirstLetterOnly(const QString& sentence) {
-    QStringList words = sentence.split(' ', Qt::SkipEmptyParts);
-    for (QString& word : words) {
-      word.front() = word.front().toUpper();
-    }
-    // ROND D'ALEMBERT => Rond D'Alembert
-    QString caurse = words.join(' ');
-    int apostrophyIndex = caurse.indexOf('\'');
-    if (apostrophyIndex != -1 and apostrophyIndex + 1 < caurse.size()) {
-      caurse[apostrophyIndex + 1] = caurse[apostrophyIndex + 1].toUpper();
-    }
-    return caurse;
-  }
-
-  static QString CapitaliseEachWordFirstLetterLowercaseOthers(const QString& sentence) {
-    const QString& sentenceInLowercase = sentence.toLower();
-    return CapitaliseEachWordFirstLetterOnly(sentenceInLowercase);
-  }
-
-  static QString ToggleSentenceCase(const QString& sentence) {
-    QString toggled;
-    for (QChar c : sentence) {
-      if (c.isLetter()) {
-        toggled += (c.toLower());  // trick upper to lower case by bitwise operator
-      } else {
-        toggled += c;
-      }
-    }
-    return toggled;
-  }
+  static QString CapitaliseEachWordFirstLetterOnly(const QString& sentence);
+  static QString CapitaliseEachWordFirstLetterLowercaseOthers(const QString& sentence);
+  static QString ToggleSentenceCase(const QString& sentence);
 
   static const char* FIELD_SEPERATOR;
   static const QRegularExpression FS_COMP;
