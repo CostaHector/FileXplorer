13142ff68d31e4d61f79bac4e8094227ec14b035
HanCosta <hancosta@outlook.com>
2025-03-25 00:18:47 +0800

    Version 60.5.6 Testcase batch run

--- TestCase/ProductionStudioManagerTest.cpp
+++ TestCase/ProductionStudioManagerTest.cpp
@@ -1,18 +1,19 @@
 #include <QCoreApplication>
 #include <QtTest>
-
+#include "pub/MyTestSuite.h"
 // add necessary includes here
 #include "Tools/ProductionStudioManager.h"
 
-class ProductionStudioManagerTest : public QObject {
+class ProductionStudioManagerTest : public MyTestSuite {
   Q_OBJECT
  public:
-  explicit ProductionStudioManagerTest(QObject* parent = nullptr) : QObject(parent), psm(ProductionStudioManager::getIns()) {}
-  ProductionStudioManager& psm;
-
  private slots:
-  void test_performersDictNotEmpty() { QVERIFY2(psm.count() > 0, "production studio hash table should not be empty"); }
+  void test_performersDictNotEmpty() {
+    const ProductionStudioManager& psm{ProductionStudioManager::getIns()};
+    QVERIFY2(psm.count() > 0, "production studio hash table should not be empty");
+  }
   void test_standardProdStudioNameFrom() {
+    const ProductionStudioManager& psm{ProductionStudioManager::getIns()};
     const QStringList& fromLst = psm.StandardProductionStudioFrom("LucasEntertainment");
     QVERIFY2(fromLst.contains("lucasentertainment"), "only lowercase edition");
     QVERIFY2(fromLst.contains("lucas entertainment"), "split by A-Z0-9 and join by space");
@@ -31,6 +32,7 @@ class ProductionStudioManagerTest : public QObject {
   }
 
   void test_filterProdStudioNameOut() {
+    const ProductionStudioManager& psm{ProductionStudioManager::getIns()};
     QCOMPARE(psm("[FFL] Lucas Entertainment - ABC.mp4"), "LucasEntertainment");
     QCOMPARE(psm("[FFL] LucasEntertainment - ABC.mp4"), "LucasEntertainment");
     QCOMPARE(psm("[FFL] lucas entertainment - ABC.mp4"), "LucasEntertainment");
@@ -41,11 +43,12 @@ class ProductionStudioManagerTest : public QObject {
     QCOMPARE(psm("[baitbus] 2008 - Part.mp4"), "BaitBus");
   }
 
-  void test_hintStudioName(){
+  void test_hintStudioName() {
+    const ProductionStudioManager& psm{ProductionStudioManager::getIns()};
     QCOMPARE(psm.hintStdStudioName("not a studio map at all - 2008 - Part.mp4"), "");
     QCOMPARE(psm.hintStdStudioName("Men at play - 2008 - Part.mp4"), "MenAtPlay");
   }
 };
 
-//QTEST_MAIN(ProductionStudioManagerTest)
-//#include "ProductionStudioManagerTest.moc"
+ProductionStudioManagerTest g_ProductionStudioManagerTest;
+#include "ProductionStudioManagerTest.moc"
