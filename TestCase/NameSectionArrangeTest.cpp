ba6106f299b12280cdab78469ec9c75782de0cee
HanCosta <hancosta@outlook.com>
2025-03-29 23:02:43 +0800

    Version 60.5.9 Arrange Name Section and testcase

--- TestCase/NameSectionArrangeTest.cpp
+++ TestCase/NameSectionArrangeTest.cpp
@@ -12,41 +12,41 @@ class NameSectionArrangeTest : public MyTestSuite {
   void init() {}
   void cleanup() {}
 
-  void arrange0213Basic() {
-    NameSectionArrange nss("0213");
+  void test_0213_1digit_SortIndex_strict() {
+    NameSectionArrange nss({0, 2, 1, 3});
 
     QString actual = nss("1-2-3-4");
     QString expect("1 - 3 - 2 - 4");
     QCOMPARE(expect, actual);
   }
 
-  void arrangeWastedPattern40123BasicStrict() {
-    NameSectionArrange nss("4012");
+  void test_4012_1digit_SortIndex_strict() {
+    NameSectionArrange nss({4, 0, 1, 2},  false);
     QString actual = nss("1-2-3-4");
-    QString expect("1-2-3-4");
+    QString expect("1 - 2 - 3");
     QCOMPARE(expect, actual);
   }
 
-  void arrangeRedundantPattern40213BasicLoose() {
-    NameSectionArrange nss("40213", false);
+  void test_40213_1digit_SortIndex() {
+    NameSectionArrange nss({4, 0, 2, 1, 3}, false);
     QString actual = nss("1-2-3-4");
     QString expect("1 - 3 - 2 - 4");
     QCOMPARE(expect, actual);
   }
 
-  void arrangeWastedSectionPattern021() {
-    NameSectionArrange nss("4021", false);
+  void test_4021_1digitSortIndex() {
+    NameSectionArrange nss{{4, 0, 2, 1}, true}; // section 3 wasted
     QStringList actual = nss.BatchSwapper({"1-2-3-4", "A-B-C-D", "X - Y - Z"});
     QStringList expect{"1 - 3 - 2", "A - C - B", "X - Z - Y"};
-    QCOMPARE(nss.m_wastedList.size(), 2);
-    QCOMPARE(expect, actual);
+    QCOMPARE(nss.GetWastedNames(), "1-2-3-4\nA-B-C-D");
+    QCOMPARE(actual, expect);
   }
 
-  void arrangeSwapper12() {
+  void test_arrangeSwapper12() {
     NameSectionArrange nss(1, 2, false);
     QStringList actual = nss.BatchSwapper({"1-2-3-4", "A-B-C-D", "X - Y - Z"});
     QStringList expect{"1 - 3 - 2 - 4", "A - C - B - D", "X - Z - Y"};
-    QCOMPARE(nss.m_wastedList.size(), 0);
+    QVERIFY(!nss.HasWasted());
     QCOMPARE(expect, actual);
   }
 };
