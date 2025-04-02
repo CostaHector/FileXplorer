b7df980629514b8e1c7f67813af9162488ba6df7
HanCosta <hancosta@outlook.com>
2025-04-02 19:05:35 +0800

    Get Rating Image by QPainter instead of read image

--- Component/RatingSqlTableModel.cpp
+++ Component/RatingSqlTableModel.cpp
@@ -1,24 +1,40 @@
 #include "RatingSqlTableModel.h"
 #include "Tools/PerformerJsonFileHelper.h"
+#include <QPainter>
+#include <QPixmap>
+
+static const int MAX_RATE = 10;
+
+QPixmap GetRatePixmap(int r, bool hasBorder = false) {
+  if (r < 0 || r > MAX_RATE) {
+    qDebug("rate[%d] out bound", r);
+    return {};
+  }
+  static constexpr int WIDTH = 100, HEIGHT = (int)(WIDTH * 0.618);
+  QPixmap mp{WIDTH, HEIGHT};
+  int originWith = WIDTH * r / 10;
+  mp.fill(QColor{0, 0, 0, 0});
+  QPainter painter{&mp};
+  painter.setPen(QColor{255, 165, 0, 255});
+  painter.setBrush(QColor{255, 165, 0, 255});
+  painter.drawRect(0, 0, originWith, HEIGHT);
+  if (hasBorder) {
+    painter.setPen(QColor{0, 0, 0, 255});
+    painter.setBrush(QColor{0, 0, 0, 0});
+    painter.drawRect(0, 0, WIDTH - 1, HEIGHT - 1);
+  }
+  painter.end();
+  return mp;
+}
 
 QVariant RatingSqlTableModel::data(const QModelIndex& index, int role) const {
-  static const QPixmap PERFORMER_SCORE_BOARD[] = {QPixmap(":img/RATE0").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE1").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE2").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE3").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE4").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE5").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE6").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE7").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE8").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE9").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation),
-                                                  QPixmap(":img/RATE10").scaled(100, 60, Qt::KeepAspectRatio, Qt::SmoothTransformation)};
-
+  static const QPixmap PERFORMER_SCORE_BOARD[MAX_RATE + 1] = {GetRatePixmap(0), GetRatePixmap(1), GetRatePixmap(2), GetRatePixmap(3), GetRatePixmap(4), GetRatePixmap(5),
+                                                              GetRatePixmap(6), GetRatePixmap(7), GetRatePixmap(8), GetRatePixmap(9), GetRatePixmap(10)};
   if (index.column() == PERFORMER_DB_HEADER_KEY::Rate_INDEX) {
     //    return QIcon(":img/RATE" + QSqlTableModel::data(index, Qt::DisplayRole).toString());
     const int sc = QSqlTableModel::data(index, Qt::DisplayRole).toInt();
     if (role == Qt::DecorationRole) {
-      return PERFORMER_SCORE_BOARD[(sc > 10) ? 10 : (sc < 0 ? 0 : sc)];
+      return PERFORMER_SCORE_BOARD[(sc > MAX_RATE) ? MAX_RATE : (sc < 0 ? 0 : sc)];
     }
   }
   return QSqlTableModel::data(index, role);
