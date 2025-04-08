97552d491c2f8decd688f73cdbcad73df0f70eb8
CostaHector <hancosta@outlook.com>
2025-04-08 23:37:19 +0800

    ExtractPileItemsOutFolder test fix

--- Tools/ExtractPileItemsOutFolder.h
+++ Tools/ExtractPileItemsOutFolder.h
@@ -7,17 +7,18 @@
 
 class ExtractPileItemsOutFolder {
  public:
-  static QMap<QString, QStringList> UnpackItemFromPiles(const QString& path);
-  static QMap<QString, QStringList> UnpackItemFromPiles(const QString& path, const QStringList& folders);
+  static QMap<QString, QStringList> GetFolder2ItemsMapByCurPath(const QString& path);
 
   static bool CanExtractOut(const QStringList& items);
   int operator()(const QString& path);
-  int operator()(const QString& path, const QMap<QString, QStringList>& folder2PileItems);
+  int operator()(const QString& path,                                 //
+                 const QMap<QString, QStringList>& folder2PileItems,  //
+                 const QStringList& alreadyExistItems);
   bool StartToRearrange();
   int CommandsCnt() const { return m_cmds.size(); }
+
  private:
   FileOperatorType::BATCH_COMMAND_LIST_TYPE m_cmds;
 };
 
-
-#endif // EXTRACTPILEITEMSOUTFOLDER_H
+#endif  // EXTRACTPILEITEMSOUTFOLDER_H
