509509b5147e367d1edbb463c12326b15ed265b7
CostaHector <hancosta@outlook.com>
2025-04-13 20:46:40 +0800

    Detail Pane

--- Component/FolderPreview/FloatingPreview.h
+++ Component/FolderPreview/FloatingPreview.h
@@ -3,14 +3,17 @@
 
 #include "Model/FloatingModels.h"
 #include "View/ItemView.h"
+#include "Tools/SplitterInsertIndexHelper.h"
+#include "ClickableTextBrowser.h"
 #include <QSplitter>
 #include <QPushButton>
 #include <QAction>
 #include <QToolBar>
-#include <QLineEdit>
 
 class FloatingPreview : public QSplitter {
  public:
+  enum PANE_TYPE { DETAIL, TYPE_DISPLAY_CONTROL, IMAGE, VIDEO, OTHER, BUTT };
+
   FloatingPreview(QWidget* parent = nullptr);
   void ReadSettings();
   void SaveSettings();
@@ -23,9 +26,44 @@ class FloatingPreview : public QSplitter {
   void UpdateOthers(const QStringList& dataLst);
 
   bool NeedUpdate(const QString& lastName) const { return !lastName.isEmpty() && (mLastName.isEmpty() || mLastName != lastName); }
+  bool NeedUpdateDetail() const { return mDetailsPane != nullptr; }
   bool NeedUpdateImgs() const { return mImgTv != nullptr; }
   bool NeedUpdateVids() const { return mVidTv != nullptr; }
   bool NeedUpdateOthers() const { return mOthTv != nullptr; }
+  void BeforeDisplayAFileDetail() {
+    if (NeedUpdateDetail() && !mDetailsPane->isVisible()) {
+      mDetailsPane->setVisible(true);
+    }
+    if (mTypeToDisplayTB != nullptr && mTypeToDisplayTB->isVisible()) {
+      mTypeToDisplayTB->setVisible(false);
+    }
+    if (_IMG_ENABLED->isChecked() && NeedUpdateImgs() && mImgTv->isVisible()) {
+      mImgTv->setVisible(false);
+    }
+    if (_VID_ENABLED->isChecked() && NeedUpdateVids() && mVidTv->isVisible()) {
+      mVidTv->setVisible(false);
+    }
+    if (_OTH_ENABLED->isChecked() && NeedUpdateOthers() && mOthTv->isVisible()) {
+      mOthTv->setVisible(false);
+    }
+  }
+  void BeforeDisplayAFolder() {
+    if (NeedUpdateDetail() && mDetailsPane->isVisible()) {
+      mDetailsPane->setVisible(false);
+    }
+    if (mTypeToDisplayTB != nullptr && !mTypeToDisplayTB->isVisible()) {
+      mTypeToDisplayTB->setVisible(true);
+    }
+    if (_IMG_ENABLED->isChecked() && NeedUpdateImgs() && !mImgTv->isVisible()) {
+      mImgTv->setVisible(true);
+    }
+    if (_VID_ENABLED->isChecked() && NeedUpdateVids() && !mVidTv->isVisible()) {
+      mVidTv->setVisible(true);
+    }
+    if (_OTH_ENABLED->isChecked() && NeedUpdateOthers() && !mOthTv->isVisible()) {
+      mOthTv->setVisible(true);
+    }
+  }
 
   void subscribe();
 
@@ -37,12 +75,15 @@ class FloatingPreview : public QSplitter {
   QAction *_IMG_ENABLED{nullptr}, *_VID_ENABLED{nullptr}, *_OTH_ENABLED{nullptr};
   QToolBar* mTypeToDisplayTB{nullptr};
 
+  ClickableTextBrowser* mDetailsPane{nullptr};
   ImgsModel* mImgModel{nullptr};
   VidsModel* mVidsModel{nullptr};
   OthersModel* mOthModel{nullptr};
   ItemView *mImgTv{nullptr}, *mVidTv{nullptr}, *mOthTv{nullptr};
 
   QString mLastName;
+
+  SplitterInsertIndexHelper m_insertIndex{BUTT};
 };
 
 #endif  // FLOATINGPREVIEW_H
--- Component/FolderPreview/PreviewBrowser.h
+++ Component/FolderPreview/PreviewBrowser.h
@@ -1,13 +1,8 @@
 #ifndef PREVIEWBROWSER_H
 #define PREVIEWBROWSER_H
-#include <QDesktopServices>
-#include <QDir>
-#include <QScrollBar>
-#include <QTextBrowser>
-#include <QTextCursor>
-#include <QTextEdit>
+#include "ClickableTextBrowser.h"
 
-class PreviewBrowser : public QTextBrowser {
+class PreviewBrowser : public ClickableTextBrowser {
  public:
   explicit PreviewBrowser(QWidget* parent = nullptr);
   auto operator()(const QString& path) -> bool;
@@ -19,8 +14,6 @@ class PreviewBrowser : public QTextBrowser {
     }
     m_parentDocker->setWindowTitle(QString::number(vidCnt) + '|' + QString::number(m_imgsLst.size()));
   }
-
-  bool onAnchorClicked(const QUrl& url);
 
   QStringList InitImgsList(const QString& dirPath) const;
   bool hasNextImgs() const;
@@ -34,12 +27,6 @@ class PreviewBrowser : public QTextBrowser {
   QString dirPath;
   QStringList m_imgsLst;
   QWidget* m_parentDocker;
-  QAction* m_PLAY_ACTION;
-  static const QString HTML_H1_TEMPLATE;
-  static const QString HTML_H1_WITH_VIDS_TEMPLATE;
-  static const QString HTML_IMG_TEMPLATE;
-  static constexpr int HTML_IMG_FIXED_WIDTH = 600;
 };
 
-void ffff();
 #endif  // PREVIEWBROWSER_H
