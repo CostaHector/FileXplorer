f66186cbfd6fbd48254dc5b807d59adf1cb9effe
CostaHector <hancosta@outlook.com>
2025-04-20 23:44:35 +0800

    Version 60.7.5 RedundantImageFinder testcase

---
+++ Tools/RedunImgLibs.h
@@ -0,0 +1,25 @@
+#ifndef REDUNIMGLIBS_H
+#define REDUNIMGLIBS_H
+#include <QSet>
+#include <QString>
+
+struct REDUNDANT_IMG_INFO {
+  QString filePath;
+  qint64 size;
+  QString md5;
+};
+
+typedef QList<REDUNDANT_IMG_INFO> REDUNDANT_IMG_BUNCH;
+
+struct RedunImgLibs {
+  RedunImgLibs(const QString& libPath="") : m_libPath{libPath} {}
+  static QString GetRedunPath();
+  int ReadLib();
+  int WriteLib();
+  int LearnSizeAndHashFromRedunImgPath(const QString& folderPath);
+  REDUNDANT_IMG_BUNCH FindRedunImgs(const QString& folderPath);
+  const QString m_libPath;
+  QSet<qint64> m_commonFileSizeSet;
+  QSet<QString> m_commonFileHash;
+};
+#endif // REDUNIMGLIBS_H
