cb73f0111a9289cb33d166831eb26314ddc9baa2
CostaHector <hancosta@outlook.com>
2025-04-20 16:01:54 +0800

    Version 60.7.4 FileSystemItemFilter testcase

--- Actions/ActionWithPath.h
+++ Actions/ActionWithPath.h
@@ -10,17 +10,16 @@ class ActionWithPath : public QAction {
   explicit ActionWithPath(const QString& path, const QString& text, QObject* parent = nullptr);
   explicit ActionWithPath(const QString& path, const QIcon& icon, const QString& text, QObject* parent = nullptr);
 
-  inline void setPath(const QString& path) { m_path = path; }
   inline QString getPath() const { return m_path; }
   bool on_PathActionTriggered();
   static void BindIntoNewPath(T_IntoNewPath IntoNewPath) { m_IntoNewPath = IntoNewPath; }
 
  private:
-  void subscribe() { connect(this, &QAction::triggered, this, &ActionWithPath::on_PathActionTriggered); }
+  void subscribe();
 
   static T_IntoNewPath m_IntoNewPath;
 
-  QString m_path;
+  const QString m_path;
 };
 
 #endif  // ACTIONWITHPATH_H
