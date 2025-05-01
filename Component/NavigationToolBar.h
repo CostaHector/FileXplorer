8995d045d4036041f1d34964fa35f9321f3be71d
CostaHector <hancosta@outlook.com>
2025-05-02 00:11:38 +0800

    NavigationExToolBar

--- Component/NavigationToolBar.h
+++ Component/NavigationToolBar.h
@@ -1,19 +1,13 @@
 #ifndef NAVIGATIONTOOLBAR_H
 #define NAVIGATIONTOOLBAR_H
 
-#include <QAction>
-#include <QActionGroup>
-#include <QList>
-#include <QToolBar>
 #include "NavigationExToolBar.h"
 
 class NavigationToolBar : public QToolBar {
  public:
-  NavigationToolBar(const QString& title = "NavigationToolBar", bool isShow_ = true);
-  void InitFixedActions();
-  void AppendExtraActions(const QMap<QString, QString>& folderName2AbsPath);
- protected:
-  NavigationExToolBar* m_extraAppendTB;
+  explicit NavigationToolBar(const QString& title = "NavigationToolBar", bool isShow_ = true);
+ private:
+  NavigationExToolBar* m_extraAppendTB{nullptr};
 };
 
 #endif  // NAVIGATIONTOOLBAR_H
