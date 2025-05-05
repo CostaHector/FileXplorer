e23a6b2854badaee754204170c2f60c9e7bbdec2
CostaHector <hancosta@outlook.com>
2025-05-05 22:22:29 +0800

    Version 60.9.0 Fix View Enum String Bug

--- public/PublicVariable.cpp
+++ public/PublicVariable.cpp
@@ -10,9 +10,9 @@ int IMAGE_SIZE::IMG_HEIGHT = 320;
 constexpr int CCMMode::MCCL2STR_MAX_LEN;
 const char CCMMode::MCCL2STR[BUTT][MCCL2STR_MAX_LEN]  //
     {
-        VOLUME_ENUM_TO_STRING(MERGE_OP),  //
-        VOLUME_ENUM_TO_STRING(COPY_OP),   //
-        VOLUME_ENUM_TO_STRING(CUT_OP),    //
-        VOLUME_ENUM_TO_STRING(LINK_OP),   //
-        VOLUME_ENUM_TO_STRING(ERROR_OP)   //
+        ENUM_TO_STRING(MERGE_OP),  //
+        ENUM_TO_STRING(COPY_OP),   //
+        ENUM_TO_STRING(CUT_OP),    //
+        ENUM_TO_STRING(LINK_OP),   //
+        ENUM_TO_STRING(ERROR_OP)   //
     };
