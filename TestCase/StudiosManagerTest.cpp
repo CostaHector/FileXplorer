ae43b016c99fbf7662f2d330f14cc7aedc24da34
CostaHector <hancosta@outlook.com>
2025-05-07 23:40:33 +0800

    Version 60.9.1 Json Key Default Value Process

--- TestCase/StudiosManagerTest.cpp
+++ TestCase/StudiosManagerTest.cpp
@@ -2,18 +2,18 @@
 #include <QtTest>
 #include "pub/MyTestSuite.h"
 // add necessary includes here
-#include "Tools/ProductionStudioManager.h"
+#include "Tools/StudiosManager.h"
 
-class ProductionStudioManagerTest : public MyTestSuite {
+class StudiosManagerTest : public MyTestSuite {
   Q_OBJECT
  public:
  private slots:
   void test_performersDictNotEmpty() {
-    const ProductionStudioManager& psm{ProductionStudioManager::getIns()};
+    const StudiosManager& psm{StudiosManager::getIns()};
     QVERIFY2(psm.count() > 0, "production studio hash table should not be empty");
   }
   void test_standardProdStudioNameFrom() {
-    const ProductionStudioManager& psm{ProductionStudioManager::getIns()};
+    const StudiosManager& psm{StudiosManager::getIns()};
     const QStringList& fromLst = psm.StandardProductionStudioFrom("LucasEntertainment");
     QVERIFY2(fromLst.contains("lucasentertainment"), "only lowercase edition");
     QVERIFY2(fromLst.contains("lucas entertainment"), "split by A-Z0-9 and join by space");
@@ -32,7 +32,7 @@ class ProductionStudioManagerTest : public MyTestSuite {
   }
 
   void test_filterProdStudioNameOut() {
-    const ProductionStudioManager& psm{ProductionStudioManager::getIns()};
+    const StudiosManager& psm{StudiosManager::getIns()};
     QCOMPARE(psm("[FFL] Lucas Entertainment - ABC.mp4"), "LucasEntertainment");
     QCOMPARE(psm("[FFL] LucasEntertainment - ABC.mp4"), "LucasEntertainment");
     QCOMPARE(psm("[FFL] lucas entertainment - ABC.mp4"), "LucasEntertainment");
@@ -44,11 +44,11 @@ class ProductionStudioManagerTest : public MyTestSuite {
   }
 
   void test_hintStudioName() {
-    const ProductionStudioManager& psm{ProductionStudioManager::getIns()};
+    const StudiosManager& psm{StudiosManager::getIns()};
     QCOMPARE(psm.hintStdStudioName("not a studio map at all - 2008 - Part.mp4"), "");
     QCOMPARE(psm.hintStdStudioName("Men at play - 2008 - Part.mp4"), "MenAtPlay");
   }
 };
 
-ProductionStudioManagerTest g_ProductionStudioManagerTest;
-#include "ProductionStudioManagerTest.moc"
+StudiosManagerTest g_StudiosManagerTest;
+#include "StudiosManagerTest.moc"
