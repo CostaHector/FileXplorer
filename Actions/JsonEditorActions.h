bc3f3f7fcf91eb453c76daa4396de3ce68a4435f
CostaHector <hancosta@outlook.com>
2025-05-09 22:50:56 +0800

    Version 60.9.2 Json Key Icon

--- Actions/JsonEditorActions.h
+++ Actions/JsonEditorActions.h
@@ -12,19 +12,18 @@ class JsonEditorActions : public QObject {
  public:
   explicit JsonEditorActions(QObject* parent = nullptr);
 
-  QMenuBar* GetJsonMenuBar(QWidget* parent);
   QToolBar* GetJsonToolBar(QWidget* parent);
   QMenu* GetJsonToBeEdittedListMenu(QWidget* parent);
 
-  QAction* _CLR_TO_BE_EDITED_LIST{nullptr};
-  QActionGroup* _FILE_LOAD_ACTIONS{nullptr};
+  QAction* _SELECT_CURRENT_FOLDER{nullptr};
+  QAction* _CLR_JSON_FILE_LIST{nullptr};
 
   QAction* _LAST_FILE{nullptr};
   QAction* _NEXT_FILE{nullptr};
+  QAction* _DONE_AND_PREVIOUS{nullptr};
   QAction* _DONE_AND_NEXT{nullptr};
   QAction* _AUTO_SKIP{nullptr};
-  QAction* _COMPLETE_PERFS_COUNT{nullptr};
-  QActionGroup* _QUICK_EDIT_ACTIONS{nullptr};
+  QAction* _SKIP_IF_CAST_CNT_GT{nullptr};
 
   QAction* _CAPITALIZE_FIRST_LETTER_OF_EACH_WORD{nullptr};
   QAction* _LOWER_ALL_WORDS{nullptr};
@@ -34,20 +33,15 @@ class JsonEditorActions : public QObject {
   QAction* _EXTRACT_CAPITALIZED_PERFORMER{nullptr};
   QActionGroup* _TEXT_EDIT_ACTIONS{nullptr};
 
-  QAction* _SAVE{nullptr};
-  QActionGroup* _FILE_SAVE_ACTIONS{nullptr};
+  QAction* _SAVE_CURRENT_CHANGES{nullptr};
 
-  QActionGroup* GetSystemActions();
   QAction* _REVEAL_IN_EXPLORER{nullptr};
   QAction* _OPEN_THIS_FILE{nullptr};
   QAction* _RENAME_THIS_FILE{nullptr};
-  QActionGroup* _SYSTEM_ACTIONS{nullptr};
 
   QAction* _AI_HINT{nullptr};
   QAction* _LEARN_PERFORMERS_FROM_JSON{nullptr};
-  QActionGroup* _AI_ACTIONS{nullptr};
 
-  QAction* _SELECT_CURRENT_FOLDER{nullptr};
   QAction* _CONSTRUCT_JSONS_FOR_VIDS{nullptr};
   QAction* _APPEND_PERFORMERS{nullptr};
   QAction* _SET_STUDIO{nullptr};
