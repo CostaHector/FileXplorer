aad77224cf6c4179c128499ca4bfce7b8a07f0f7
CostaHector <hancosta@outlook.com>
2025-05-10 11:58:47 +0800

    Version 60.9.4 ReplaceAndUpdateSelection

--- Tools/NameTool.cpp
+++ Tools/NameTool.cpp
@@ -20,15 +20,17 @@ QStringList NameTool::operator()(const QString& s) const {
   ans.removeDuplicates();
   return ans;
 }
-QStringList NameTool::fromArticleCapitalizedNames(const QString& s) const {
+QStringList NameTool::castFromTitledSentence(const QString& s) const {
   QRegularExpressionMatchIterator it = NAME_COMP.globalMatch(s);
   QStringList ans;
+  QString srcCastName, stdCastName;
+  QRegularExpressionMatch match;
   while (it.hasNext()) {
-    QRegularExpressionMatch match = it.next();
+    match = it.next();
     if (match.hasMatch()) {
-      const QString& caurseName = match.captured(0);
-      const QString& stdName = CapitaliseFirstLetterLowerOther(caurseName);
-      ans.append(stdName);
+      srcCastName = match.captured(0);
+      stdCastName = CapitaliseFirstLetterLowerOther(srcCastName);
+      ans.append(stdCastName);
     }
   }
   ans.removeDuplicates();
@@ -43,7 +45,7 @@ QString NameTool::CapitaliseFirstLetterKeepOther(const QString& sentence) {
   // ROND D'ALEMBERT => Rond D'Alembert
   QString caurse = words.join(' ');
   int apostrophyIndex = caurse.indexOf('\'');
-  if (apostrophyIndex != -1 and apostrophyIndex + 1 < caurse.size()) {
+  if (apostrophyIndex != -1 && apostrophyIndex + 1 < caurse.size()) {
     caurse[apostrophyIndex + 1] = caurse[apostrophyIndex + 1].toUpper();
   }
   return caurse;
@@ -64,4 +66,51 @@ QString NameTool::ToggleSentenceCase(const QString& sentence) {
     }
   }
   return toggled;
+}
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QTextDocumentFragment>
+bool NameTool::ReplaceAndUpdateSelection(QTextEdit& te, SentenceProcessorFunc fTrans) {
+  if (fTrans == nullptr) {
+    qWarning("fTrans is nullptr");
+    return false;
+  }
+  if (!te.textCursor().hasSelection()) {
+    qDebug("skip no text selected in QTextEdit");
+    return true;
+  }
+
+  QTextCursor curSelection = te.textCursor();
+  const int startPos = curSelection.selectionStart();
+  const QString& before = curSelection.selection().toPlainText();
+  curSelection.removeSelectedText();
+  const QString& after = fTrans(before);
+  curSelection.insertText(after);
+  const int endPos = startPos + after.size();
+  curSelection.setPosition(startPos);
+  curSelection.setPosition(endPos, QTextCursor::KeepAnchor);
+  te.setTextCursor(curSelection);
+  return true;
+}
+
+bool NameTool::ReplaceAndUpdateSelection(QLineEdit& le, SentenceProcessorFunc fTrans) {
+  if (fTrans == nullptr) {
+    qWarning("fTrans is nullptr");
+    return false;
+  }
+  if (!le.hasSelectedText()) {
+    qDebug("skip no text selected in QLineEdit");
+    return true;
+  }
+
+  const int startPos = le.selectionStart();
+  const int endPos = le.selectionEnd();
+  const QString& before = le.selectedText();
+  const QString& after = fTrans(before);
+  const QString& beforeFullText = le.text();
+  const QString& afterFullText = beforeFullText.left(startPos) + after + beforeFullText.mid(endPos);
+  le.setText(afterFullText);
+  const int newSelectLetterCnt = after.size();
+  le.setSelection(startPos, newSelectLetterCnt);
+  return true;
 }
