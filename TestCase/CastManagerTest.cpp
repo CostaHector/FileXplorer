6d334c40f6845bef1f9d9314e722bb1a1446d3a6
CostaHector <hancosta@outlook.com>
2025-05-11 10:50:40 +0800

    Version 60.9.8 Cast/Studio Manger Update Cache bug fixed

--- TestCase/CastManagerTest.cpp
+++ TestCase/CastManagerTest.cpp
@@ -1,16 +1,24 @@
 #include <QCoreApplication>
 #include <QtTest>
+#include "TestCase/pub/GlbDataProtect.h"
+#include "TestCase/pub/OnScopeExit.h"
+#include "TestCase/PathRelatedTool.h"
+#include "Tools/JsonFileHelper.h"
 #include "pub/MyTestSuite.h"
 
 // add necessary includes here
 #include "Tools/CastManager.h"
+#include "Tools/StudiosManager.h"
+#include "public/PublicMacro.h"
+const QString rootpath = TestCaseRootPath() + "/test/TestEnv_JsonCastStudio";
+
 class CastManagerTest : public MyTestSuite {
   Q_OBJECT
-public:
+ public:
   CastManagerTest() : MyTestSuite{false}, pm{CastManager::getIns()} {}
   const CastManager& pm;
 
-private slots:
+ private slots:
   void test_performersDictNotEmpty() {  //
     QVERIFY2(!pm.m_performers.isEmpty(), "performers list should not be empty");
   }
@@ -67,11 +75,61 @@ private slots:
     QVERIFY(pm.m_performers.contains("craig kennedy"));
     QVERIFY(pm.m_performers.contains("gabriel clark"));
     const auto& wordsList = pm.SplitSentence("britishtwunk\r\nCraig Kennedy\nGabriel Clark");
-    QVERIFY2(not wordsList.isEmpty(), "words list should not empty");
+    QVERIFY2(!wordsList.isEmpty(), "words list should not empty");
 
     const auto& perfsList = pm.FilterPerformersOut(wordsList);
     const QStringList expectCasts{"britishtwunk", "Craig Kennedy", "Gabriel Clark"};
     QCOMPARE(perfsList, expectCasts);
+  }
+
+  void test_LearningFromAPath_no_write_localfile() {
+    // precondition
+    QDir dir{rootpath};
+    const QString jsonAbsFile{dir.absoluteFilePath("My Good Boy.json")};
+    QVERIFY(QFile::exists(jsonAbsFile));
+
+    using namespace JSON_KEY;
+    const QVariantHash& dict = JsonFileHelper::MovieJsonLoader(jsonAbsFile);
+
+    const auto studioIt = dict.constFind(ENUM_TO_STRING(Studio));
+    QVERIFY(studioIt != dict.cend());
+    const QString& studioName{"StudioNotExist"};
+    // studio not exist, studionotexist => StudioNotExist
+    QHash<QString, QString> expectStudioSet{{"studionotexist", "StudioNotExist"},  //
+                                            {"studio not exist", "StudioNotExist"}};
+    QCOMPARE(studioIt.value().toString(), studioName);
+
+    const auto castIt = dict.constFind(ENUM_TO_STRING(Cast));
+    QVERIFY(castIt != dict.cend());
+    const QStringList& castLst{"Cast1NotExist", "Cast2NotExist"};
+    QSet<QString> expectCastSet{"cast1notexist", "cast2notexist"};
+    QCOMPARE(castIt.value().toStringList(), castLst);
+
+    // procedure
+    auto& studioInst = StudiosManager::getIns();
+    QHash<QString, QString> emptyStudioSet;
+    studioInst.m_prodStudioMap.swap(emptyStudioSet);
+    ON_SCOPE_EXIT {
+      studioInst.m_prodStudioMap.swap(emptyStudioSet);
+    };
+
+    auto& castInst = CastManager::getIns();
+    QSet<QString> emptyCastSet;
+    castInst.m_performers.swap(emptyCastSet);
+    ON_SCOPE_EXIT {
+      castInst.m_performers.swap(emptyCastSet);
+    };
+
+    // cached should updated, local file should not write
+    bool studioLocalFileWrite{true};
+    QCOMPARE(studioInst.LearningFromAPath(rootpath, false, &studioLocalFileWrite), 2);
+    QCOMPARE(studioInst.m_prodStudioMap, expectStudioSet);
+    QCOMPARE(studioLocalFileWrite, false);
+
+    bool castLocalFileWrite{true};
+    QCOMPARE(castInst.LearningFromAPath(rootpath, false, &castLocalFileWrite), 2);
+    QCOMPARE(castInst.m_performers, expectCastSet);
+    QCOMPARE(castLocalFileWrite, false);
   }
 };
 
