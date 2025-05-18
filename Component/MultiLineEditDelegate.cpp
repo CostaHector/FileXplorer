25dbf95f318972aeff36d80b8913305068aaed1f
CostaHector <hancosta@outlook.com>
2025-05-18 19:53:14 +0800

    61.0.9 Json Table View Detail PlainTextEdit Delegate

---
+++ Component/MultiLineEditDelegate.cpp
@@ -0,0 +1,26 @@
+#include "MultiLineEditDelegate.h"
+#include <QPlainTextEdit>
+
+MultiLineEditDelegate::MultiLineEditDelegate(QWidget *parent)
+    : QStyledItemDelegate(parent)
+{
+
+}
+
+QWidget *MultiLineEditDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+  QPlainTextEdit *editor = new QPlainTextEdit(parent);
+  return editor;
+}
+
+void MultiLineEditDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
+{
+  QPlainTextEdit *mEditor = qobject_cast<QPlainTextEdit *>(editor);
+  mEditor->setPlainText(index.data().toString());
+}
+
+void MultiLineEditDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const
+{
+  QPlainTextEdit *mEditor = qobject_cast<QPlainTextEdit *>(editor);
+  model->setData(index, mEditor->toPlainText());
+}
