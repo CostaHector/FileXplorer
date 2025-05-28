e7818d802caed57bcdb68e711196746240a39f19
CostaHector <hancosta@outlook.com>
2025-05-28 21:46:38 +0800

    Version 60.1.7 RenameWidget_LongPath Unified with AdvanceRename

--- Tools/LongPathFinder.cpp
+++ Tools/LongPathFinder.cpp
@@ -1,80 +1,41 @@
 #include "LongPathFinder.h"
-#include <QDir>
-#include <QDirIterator>
-#include <QFileInfo>
 
 constexpr int LongPathFinder::MAX_PATH_LENGTH;
 
+bool LongPathFinder::IsTooLong(const QString& pth) {
+  return pth.size() >= MAX_PATH_LENGTH;
+}
+
+bool LongPathFinder::IsTooLong(int pathLength) {
+  return pathLength >= MAX_PATH_LENGTH;
+}
+
+QStringList LongPathFinder::GetNamesAfterSectionDropped(const QStringList& srcLst) const {
+  QStringList dst;
+  dst.reserve(srcLst.size());
+  for (const QString& oldFolderName : srcLst) {
+    dst.append(GetNewFolderName(oldFolderName));
+  }
+  return dst;
+}
+
 void LongPathFinder::SetDropSectionWhenTooLong(int section) {
-  dropSection = section;
-  if (pres.size() != olds.size() || olds.size() != news.size()) {
-    qWarning("length not match[%d, %d, %d]", pres.size(), olds.size(), news.size());
-    return;
-  }
-  if (pres.size() == 0) {
-    return;
-  }
-  news.clear();
-  for (const QString& oldFolderName : olds) {
-    const QString& newFolderName = GetNewFolderName(oldFolderName);
-    news.append(newFolderName);
-  }
+  mDropSection = section;
 }
 
 QString LongPathFinder::GetNewFolderName(const QString& oldName) const {
-  auto oldNameSectionsList = oldName.split(" - ");
-  if (oldNameSectionsList.size() <= 1 || oldNameSectionsList.size() < std::abs(dropSection)) {
+  if (mDropSection == 0) {  // 0: no drop
     return oldName;
   }
-  oldNameSectionsList.erase(oldNameSectionsList.end() + dropSection);
+  auto oldNameSectionsList = oldName.split(" - ");
+  if (oldNameSectionsList.size() <= 1 || oldNameSectionsList.size() < std::abs(mDropSection)) {
+    return oldName;
+  }
+  if (mDropSection > 0) {  // 1 : drop first section, 2 drop second section
+    oldNameSectionsList.erase(oldNameSectionsList.begin() + mDropSection - 1);
+  } else {  // -1: drop last section; -2: drop second to last second
+    oldNameSectionsList.erase(oldNameSectionsList.end() + mDropSection);
+  }
   return oldNameSectionsList.join(" - ");
 }
 
-int LongPathFinder::operator()(const QString& path) {
-  pres.clear();
-  olds.clear();
-  news.clear();
-  QFileInfo fi(path);
-  if (not fi.exists()) {
-    qWarning("file not exists[%s], skip", qPrintable(path));
-    return 0;
-  }
-  if (fi.isFile()) {
-    qDebug("skip file[%s]", qPrintable(path));
-    return 0;
-  }
-  int overlongCnt = 0;
-  QDirIterator it(path, {}, QDir::Filter::Dirs | QDir::Filter::NoDotAndDotDot, QDirIterator::IteratorFlag::Subdirectories);
-  while (it.hasNext()) {
-    const QString& currentPath = it.next();
-    if (IsLengthBelowSysLimit(currentPath)) {
-      continue;
-    }
-    const QString& oldFolderName = QFileInfo(currentPath).fileName();
-    const QString& newFolderName = GetNewFolderName(oldFolderName);
-    pres.append(it.fileInfo().absolutePath());
-    olds.append(oldFolderName);
-    news.append(newFolderName);
-    ++overlongCnt;
-  }
-  qDebug("%d folder(s) path too long", overlongCnt);
-  return overlongCnt;
-}
-
-int LongPathFinder::CheckTooLongPathCount() {
-  m_status.clear();
-  if (pres.size() != olds.size() || olds.size() != news.size()) {
-    qWarning("length not match[%d, %d, %d]", pres.size(), olds.size(), news.size());
-    return 0;
-  }
-  m_status.reserve(pres.size());
-  // len(pre) + 1 + len(newName) + 1 + len(oldName) + 5 < MAX_PATH_LENGTH
-  for (int i = 0; i < pres.size(); ++i) {
-    if (pres[i].size() + news[i].size() + olds[i].size() + 7 < MAX_PATH_LENGTH) {
-      m_status += '\n';
-      continue;
-    }
-    m_status += "x\n";
-  }
-  return m_status.size() - pres.size();
-}
