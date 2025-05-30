b5aa43f36ad0870bdbc48e6694ab2a8cd6f0ebdd
CostaHector <hancosta@outlook.com>
2025-05-30 23:08:06 +0800

    Version 60.2.0 Advance Rename Refactor

--- Component/RenameWidgets/AdvanceRenamer.cpp
+++ Component/RenameWidgets/AdvanceRenamer.cpp
@@ -7,10 +7,14 @@
 #include "Component/Notificator.h"
 #include "Tools/RenameNamesUnique.h"
 
+constexpr char AdvanceRenamer::NAME_SEP;
+
 AdvanceRenamer::AdvanceRenamer(QWidget* parent)  //
-    : QDialog(parent), windowTitleFormat("%1 | %2") {
-  m_extensionInNameCB = new (std::nothrow) QCheckBox{"Extension in name"};
-  CHECK_NULLPTR_RETURN_VOID(m_extensionInNameCB)
+    : QDialog{parent},                           //
+      windowTitleFormat("%1 | %2")               //
+{
+  m_nameExtIndependent = new (std::nothrow) QCheckBox{"Name Ext Independent"};
+  CHECK_NULLPTR_RETURN_VOID(m_nameExtIndependent)
   m_recursiveCB = new (std::nothrow) QCheckBox{"Recursive"};
   CHECK_NULLPTR_RETURN_VOID(m_recursiveCB)
   regexValidLabel = new (std::nothrow) StateLabel{"Regex expression state"};
@@ -29,27 +33,84 @@ AdvanceRenamer::AdvanceRenamer(QWidget* parent)  //
   CHECK_NULLPTR_RETURN_VOID(m_buttonBox)
   m_commandsPreview = new (std::nothrow) QPlainTextEdit;
   CHECK_NULLPTR_RETURN_VOID(m_commandsPreview)
-  // Qt.FramelessWindowHint|Qt.WindowSystemMenuHint;
-  setWindowFlag(Qt::WindowMaximizeButtonHint);  // WindowMinMaxButtonsHint;
 
-  m_extensionInNameCB->setToolTip("Extension in file name.\nRules will also work on suffix");
-  m_extensionInNameCB->setChecked(PreferenceSettings().value(MemoryKey::RENAMER_INCLUDING_FILE_EXTENSION.name, MemoryKey::RENAMER_INCLUDING_FILE_EXTENSION.v).toBool());
-  m_recursiveCB->setToolTip("Recursive rename.\nRules will also work on itself and its subdirectories");
+  m_nameExtIndependent->setToolTip(
+      "Show file base name and extension respectively.\n"
+      "So rename rules will work/or not work on extension");
+  m_nameExtIndependent->setChecked(PreferenceSettings().value(MemoryKey::RENAMER_INCLUDING_FILE_EXTENSION.name, MemoryKey::RENAMER_INCLUDING_FILE_EXTENSION.v).toBool());
+
+  m_recursiveCB->setToolTip(
+      "Recursive rename.\n"
+      "Rules will also work on itself and its subdirectories");
   m_recursiveCB->setChecked(PreferenceSettings().value(MemoryKey::RENAMER_INCLUDING_DIR.name, MemoryKey::RENAMER_INCLUDING_DIR.v).toBool());
 
   m_buttonBox->setOrientation(Qt::Orientation::Horizontal);
-  QPushButton* pOkBtn = m_buttonBox->button(QDialogButtonBox::Ok);
+  auto* pOkBtn = m_buttonBox->button(QDialogButtonBox::Ok);
   pOkBtn->setShortcut(QKeySequence(Qt::Key::Key_F10));
-  pOkBtn->setToolTip(QString("<b>%1 (%2)</b><br/> Apply changes right now.").arg(pOkBtn->text(), pOkBtn->shortcut().toString()));
+  pOkBtn->setToolTip(QString("<b>%1 (%2)</b><br/> Apply changes right now.")  //
+                         .arg(pOkBtn->text(), pOkBtn->shortcut().toString()));
   pOkBtn->setStyleSheet(SUBMIT_BTN_STYLE);
   m_buttonBox->button(QDialogButtonBox::Help)->setText("See commands...");
 
+  m_mainLayout = new (std::nothrow) QVBoxLayout();
+  CHECK_NULLPTR_RETURN_VOID(m_mainLayout);
+
+  m_relNameTE->setReadOnly(true);
+  m_relNameTE->setWordWrapMode(QTextOption::WrapMode::NoWrap);
+  m_relNameTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  m_relNameTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+  m_oBaseTE->setReadOnly(true);
+  m_oBaseTE->setWordWrapMode(QTextOption::WrapMode::NoWrap);
+  m_oBaseTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  m_oBaseTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+  m_oExtTE->setReadOnly(true);
+  m_oExtTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  m_oExtTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+  m_nBaseTE->setReadOnly(false);
+  m_nBaseTE->setWordWrapMode(QTextOption::WrapMode::NoWrap);
+  m_nBaseTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+  m_nBaseTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+  m_nExtTE->setReadOnly(false);
+  m_nExtTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+  m_nExtTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+
+  m_nameEditLayout = new (std::nothrow) QHBoxLayout;
+  CHECK_NULLPTR_RETURN_VOID(m_nameEditLayout);
+  m_nameEditLayout->addWidget(m_relNameTE);
+  m_nameEditLayout->addWidget(m_oBaseTE);
+  m_nameEditLayout->addWidget(m_oExtTE);
+  m_nameEditLayout->addWidget(m_nBaseTE);
+  m_nameEditLayout->addWidget(m_nExtTE);
+  m_nameEditLayout->setStretch(0, 3);
+  m_nameEditLayout->setStretch(1, 8);
+  m_nameEditLayout->setStretch(2, 1);
+  m_nameEditLayout->setStretch(3, 8);
+  m_nameEditLayout->setStretch(4, 1);
+  m_nameEditLayout->setSpacing(0);
+  m_nameEditLayout->setContentsMargins(0, 0, 0, 0);
+
+  const bool bNameExtIndependent{m_nameExtIndependent->checkState() == Qt::Checked};
+  m_oExtTE->setVisible(bNameExtIndependent);
+  m_nExtTE->setVisible(bNameExtIndependent);
+
+  m_mainLayout->addLayout(m_nameEditLayout);
+  m_mainLayout->addWidget(m_buttonBox);
+  setLayout(m_mainLayout);
+
+  Subscribe();
   ReadSettings();
+
+  // Qt.FramelessWindowHint|Qt.WindowSystemMenuHint;
+  setWindowFlag(Qt::WindowMaximizeButtonHint);  // WindowMinMaxButtonsHint;
 }
 
 void AdvanceRenamer::closeEvent(QCloseEvent* event) {
   PreferenceSettings().setValue("ADVANCE_RENAMER_GEOMETRY", saveGeometry());
-  return QDialog::closeEvent(event);
+  QDialog::closeEvent(event);
 }
 
 void AdvanceRenamer::ReadSettings() {
@@ -63,63 +124,13 @@ void AdvanceRenamer::ReadSettings() {
 void AdvanceRenamer::init() {
   InitExtraCommonVariable();
   InitExtraMemberWidget();
-
-  m_replaceControlBar = InitControlTB();
-
-  m_relNameTE->setWordWrapMode(QTextOption::WrapMode::NoWrap);
-  m_relNameTE->setReadOnly(true);
-  m_relNameTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  m_relNameTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-
-  m_oBaseTE->setWordWrapMode(QTextOption::WrapMode::NoWrap);
-  m_oBaseTE->setReadOnly(true);
-  m_oBaseTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  m_oBaseTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-
-  m_oExtTE->setReadOnly(true);
-  m_oExtTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  m_oExtTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-
-  m_nBaseTE->setWordWrapMode(QTextOption::WrapMode::NoWrap);
-  m_nBaseTE->setReadOnly(false);
-  m_nBaseTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-  m_nBaseTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-
-  m_nExtTE->setReadOnly(false);
-  m_nExtTE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  m_nExtTE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-
-  m_nameEditLayout = new QHBoxLayout;
-  m_nameEditLayout->addWidget(m_relNameTE);
-  m_nameEditLayout->addWidget(m_oBaseTE);
-  m_nameEditLayout->addWidget(m_oExtTE);
-  m_nameEditLayout->addWidget(m_nBaseTE);
-  m_nameEditLayout->addWidget(m_nExtTE);
-
-  m_nameEditLayout->setStretch(0, 3);
-  m_nameEditLayout->setStretch(1, 8);
-  m_nameEditLayout->setStretch(2, 1);
-  m_nameEditLayout->setStretch(3, 8);
-  m_nameEditLayout->setStretch(4, 1);
-  m_nameEditLayout->setSpacing(0);
-  m_nameEditLayout->setContentsMargins(0, 0, 0, 0);
-
-  m_mainLayout = new QVBoxLayout();
-  m_mainLayout->addWidget(m_replaceControlBar);
-  m_mainLayout->addLayout(m_nameEditLayout);
-  m_mainLayout->addWidget(m_buttonBox);
-
-  setLayout(m_mainLayout);
-  Subscribe();
+  m_controlBar = InitControlTB();
+  m_mainLayout->insertWidget(0, m_controlBar);
   extraSubscribe();
-
-  const bool isNameIncludingExtension = m_extensionInNameCB->checkState() == Qt::Checked;
-  m_oExtTE->setVisible(!isNameIncludingExtension);
-  m_nExtTE->setVisible(!isNameIncludingExtension);
 }
 
 void AdvanceRenamer::Subscribe() {
-  connect(m_extensionInNameCB, &QCheckBox::stateChanged, this, &AdvanceRenamer::onIncludeSuffix);
+  connect(m_nameExtIndependent, &QCheckBox::stateChanged, this, &AdvanceRenamer::onNameExtRespective);
   connect(m_recursiveCB, &QCheckBox::stateChanged, this, &AdvanceRenamer::onIncludingSub);
 
   connect(m_nBaseTE->verticalScrollBar(), &QScrollBar::valueChanged, this, [this](const int position) {
@@ -135,14 +146,14 @@ void AdvanceRenamer::Subscribe() {
 }
 
 bool AdvanceRenamer::onApply(const bool isOnlyHelp, const bool isInterative) {
-  const QStringList& relNameList = m_relNameTE->toPlainText().split('\n');
-  const QStringList& oldCompleteNameList = m_oBaseTE->toPlainText().split('\n');
-  const QStringList& oldSuffixList = m_oExtTE->toPlainText().split('\n');
+  const QStringList& relNameList = m_relNameTE->toPlainText().split(NAME_SEP);
+  const QStringList& oldCompleteNameList = m_oBaseTE->toPlainText().split(NAME_SEP);
+  const QStringList& oldSuffixList = m_oExtTE->toPlainText().split(NAME_SEP);
 
-  const QStringList& newCompleteNameList = m_nBaseTE->toPlainText().split('\n');
-  const QStringList& newSuffixList = m_nExtTE->toPlainText().split('\n');
+  const QStringList& newCompleteNameList = m_nBaseTE->toPlainText().split(NAME_SEP);
+  const QStringList& newSuffixList = m_nExtTE->toPlainText().split(NAME_SEP);
 
-  RenameNamesUnique renameHelper{m_pre, relNameList, oldCompleteNameList, oldSuffixList, newCompleteNameList, newSuffixList, m_recursiveCB->isChecked()};
+  RenameNamesUnique renameHelper{mWorkPath, relNameList, oldCompleteNameList, oldSuffixList, newCompleteNameList, newSuffixList, m_recursiveCB->isChecked()};
   renameHelper();
   if (!renameHelper) {
     const QString& rejectMsg = renameHelper.Details();
@@ -181,88 +192,59 @@ void AdvanceRenamer::onRegex(const int regexState) {
 void AdvanceRenamer::onIncludingSub(int includingSubState) {
   const bool isIncludingDir = includingSubState == Qt::Checked;
   PreferenceSettings().setValue(MemoryKey::RENAMER_INCLUDING_DIR.name, isIncludingDir);
-  InitTextContent(m_pre, rels);
+  InitTextEditContent(mWorkPath, mSelectedNames);
 }
 
-void AdvanceRenamer::onIncludeSuffix(int includingSuffixState) {
-  const bool isNameIncludingExtension = includingSuffixState == Qt::Checked;
-  PreferenceSettings().setValue(MemoryKey::RENAMER_INCLUDING_FILE_EXTENSION.name, isNameIncludingExtension);
-  m_oExtTE->setVisible(!isNameIncludingExtension);
-  m_nExtTE->setVisible(!isNameIncludingExtension);
-  InitTextContent(m_pre, rels);
+void AdvanceRenamer::onNameExtRespective(int includingSuffixState) {
+  const bool bNameExtIndependent{includingSuffixState == Qt::Checked};
+  PreferenceSettings().setValue(MemoryKey::RENAMER_INCLUDING_FILE_EXTENSION.name, bNameExtIndependent);
+  m_oExtTE->setVisible(bNameExtIndependent);
+  m_nExtTE->setVisible(bNameExtIndependent);
+  if (bNameExtIndependent) {
+    for (int i = 0; i < mExts.size(); ++i) {
+      std::tie(mNames[i], mExts[i]) = PathTool::GetBaseNameExt(mNames[i]);
+    }
+  } else {
+    for (int i = 0; i < mExts.size(); ++i) {
+      mNames[i] += mExts[i];
+      mExts[i].clear();
+    }
+  }
+  UpdateNameAndExt();
 }
 
-void AdvanceRenamer::InitTextContent(const QString& p, const QStringList& r) {
-  static constexpr char NAME_SEP = '\n';
+void AdvanceRenamer::UpdateNameAndExt() {
+  const QString& suffixs = mExts.join(NAME_SEP);
+  m_oBaseTE->setPlainText(mNames.join(NAME_SEP));
+  m_oExtTE->setPlainText(suffixs);
 
-  m_pre = p;  // will not change
-  rels = r;   // will never change
-
-  const bool bSubDir = m_recursiveCB->isChecked();
-  const bool bSuffixInside = m_extensionInNameCB->isChecked();
-
-  FileOsWalker osWalker{m_pre, bSuffixInside};
-  osWalker(rels, bSubDir);
-  FilterNames(osWalker);
-
-  const QStringList& relToNames = osWalker.relToNames;
-  completeNames.swap(osWalker.completeNames);
-  const QString& suffixLines = osWalker.suffixs.join(NAME_SEP);
-
-  m_relNameTE->setPlainText(relToNames.join(NAME_SEP));
-
-  m_oBaseTE->setPlainText(completeNames.join(NAME_SEP));
-  m_oExtTE->setPlainText(suffixLines);
-
-  const auto& newCompleteNames = RenameCore(completeNames);
+  const auto& newCompleteNames = RenameCore(mNames);
   m_nBaseTE->setPlainText(newCompleteNames.join(NAME_SEP));
-  m_nExtTE->setPlainText(suffixLines);
+  m_nExtTE->setPlainText(suffixs);
+}
 
-  setWindowTitle(windowTitleFormat.arg(completeNames.size()).arg(m_pre));
+void AdvanceRenamer::InitTextEditContent(const QString& workPath, const QStringList& selectedNames) {
+  mWorkPath = workPath;            // will never change
+  mSelectedNames = selectedNames;  // will never change
+
+  const bool bSubDir{m_recursiveCB->isChecked()};
+  const bool bSuffixInsideFilename{!m_nameExtIndependent->isChecked()};
+
+  FileOsWalker osWalker{mWorkPath, bSuffixInsideFilename};
+  osWalker(mSelectedNames, bSubDir);
+  FilterNames(osWalker);
+  mNames.swap(osWalker.completeNames);
+  mExts.swap(osWalker.suffixs);
+  m_relNameTE->setPlainText(osWalker.relToNames.join(NAME_SEP));
+
+  UpdateNameAndExt();
+
+  setWindowTitle(windowTitleFormat.arg(mNames.size()).arg(mWorkPath));
 }
 
 void AdvanceRenamer::OnlyTriggerRenameCore() {
-  // will not call OSWalker.;
-  // only update complete name;
-  setWindowTitle(windowTitleFormat.arg(completeNames.size()).arg(m_pre));
-  const auto& newCompleteNames = RenameCore(completeNames);
-  m_nBaseTE->setPlainText(newCompleteNames.join('\n'));
+  // will not call OSWalker; only update complete name;
+  setWindowTitle(windowTitleFormat.arg(mNames.size()).arg(mWorkPath));
+  const auto& newCompleteNames = RenameCore(mNames);
+  m_nBaseTE->setPlainText(newCompleteNames.join(NAME_SEP));
 }
-
-// #define __NAME__EQ__MAIN__ 1
-#ifdef __NAME__EQ__MAIN__
-#include <QApplication>
-
-int main(int argc, char* argv[]) {
-  QDir dir(QFileInfo(QFileInfo(__FILE__).absolutePath()).absolutePath());
-  QStringList rels = dir.entryList(QDir::Filter::AllEntries | QDir::Filter::NoDotAndDotDot, QDir::SortFlag::DirsFirst);
-
-  QApplication a(argc, argv);
-  RenameWidget_Insert winI;
-  RenameWidget_Replace winR;
-  RenameWidget_Delete winD;
-  RenameWidget_Numerize winN;
-  RenameWidget_Case winC;
-
-  winI.init();
-  winR.init();
-  winD.init();
-  winN.init();
-  winC.init();
-
-  winI.InitTextContent(dir.absolutePath(), rels);
-  winR.InitTextContent(dir.absolutePath(), rels);
-  winD.InitTextContent(dir.absolutePath(), rels);
-  winN.InitTextContent(dir.absolutePath(), rels);
-  winC.InitTextContent(dir.absolutePath(), rels);
-  // winR.InitTextContent(pre, rels)
-  // winD.InitTextContent(pre, rels)
-  // winN.InitTextContent(pre, rels)
-  winI.show();
-  winR.show();
-  winD.show();
-  winN.show();
-  winC.show();
-  return a.exec();
-}
-#endif
