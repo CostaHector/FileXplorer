1fd10541c79090fcf0111f7ac953399c5f65ffcb
CostaHector <hancosta@outlook.com>
2025-06-07 17:17:16 +0800

    Version 60.4.7 FileOsWalker/MimeDataCX linux support

---
+++ public/FileOsWalker.h
@@ -0,0 +1,25 @@
+﻿#ifndef FILEOSWALKER_H
+#define FILEOSWALKER_H
+
+#include <QStringList>
+class QFileInfo;
+
+class FileOsWalker {
+ public:
+  FileOsWalker(const QString& pre, bool sufInside);
+  void operator()(const QStringList& rels, const bool includingSub);
+  inline int size() const { return completeNames.size(); };
+
+  QStringList relToNames;
+  QStringList completeNames;
+  QStringList suffixs;
+  QList<bool> isFiles;
+
+  const QString mPrepathWithSlash;
+  const int N;
+
+ private:
+  void FillByFileInfo(const QFileInfo& fi);
+  const bool mSufInside;
+};
+#endif // FILEOSWALKER_H
