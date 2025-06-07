2dec7f68807fbd0438d3a7ccf010fd57bfc0c21c
CostaHector <hancosta@outlook.com>
2025-06-07 10:28:04 +0800

    Version 60.4.6 Advance Search Model Rerfactor

--- Tools/SearchTools.h
+++ Tools/SearchTools.h
@@ -1,4 +1,4 @@
-#ifndef SEARCHTOOLS_H
+﻿#ifndef SEARCHTOOLS_H
 #define SEARCHTOOLS_H
 
 #include <QString>
@@ -9,17 +9,16 @@ namespace SearchTools {
 enum class SEARCH_MODE : unsigned char {
   MODE_BEGIN = 0,
   NORMAL = MODE_BEGIN,
-  WILDCARD,
   REGEX,
-  SEARCH_FOR_FILE_CONTENTS,
+  FILE_CONTENTS,
   MODE_BUTT,
 };
 
 SEARCH_MODE GetSearchModeEnum(const QString& searchMode);
-
 SEARCH_MODE GetSearchModeEnum(unsigned char searchMode);
-
-QStringList GetSearchModeStrList();
+QString GetSearchModeStr(SEARCH_MODE searchMode);
+QString GetSearchModeStr(unsigned char searchMode);
+QString GetDefaultSearchModeStr();
 };  // namespace SearchTools
 
 #endif  // SEARCHTOOLS_H
