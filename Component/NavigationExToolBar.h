8a2b9b7e8cd887522be219d829b00d5f42ca7adc
CostaHector <hancosta@outlook.com>
2025-06-20 00:38:09 +0800

    Version 60.6.1 Left Navigation Actions Sort/Rearrange support

--- Component/NavigationExToolBar.h
+++ Component/NavigationExToolBar.h
@@ -1,14 +1,17 @@
-#ifndef NAVIGATIONEXTOOLBAR_H
+﻿#ifndef NAVIGATIONEXTOOLBAR_H
 #define NAVIGATIONEXTOOLBAR_H
 
+#include "public/PublicVariable.h"
 #include <QDragEnterEvent>
 #include <QDragMoveEvent>
 #include <QDropEvent>
 #include <QToolBar>
 
 class NavigationExToolBar : public QToolBar {
+  Q_OBJECT
  public:
   explicit NavigationExToolBar(const QString& title, QWidget* parent = nullptr);
+  void addDraggableAction(QAction* act);
 
   void dragEnterEvent(QDragEnterEvent* event) override;
   void dropEvent(QDropEvent* event) override;
@@ -24,13 +27,21 @@ class NavigationExToolBar : public QToolBar {
   void CustomContextMenuEvent(const QPoint& pnt);
 
   void AlighLeft();
-
   void AppendExtraActions(const QMap<QString, QString>& folderName2AbsPath);
+  static void BindIntoNewPath(T_IntoNewPath IntoNewPath) { m_IntoNewPath = IntoNewPath; }
+  static bool onPathActionTriggered(const QAction* pAct);
+
+ signals:
+  void widgetMoved(int fromIndex, int destIndex);
 
  private:
+  bool MoveToolbuttonInToolBar(int fromIndex, int destIndex);
+  static T_IntoNewPath m_IntoNewPath;
+
   QPoint mRightClickAtPnt;
-  QAction *UNPIN_THIS, *UNPIN_ALL;
-  QMenu* mMenu;
+  QPoint mDragStartPosition;
+  QAction *UNPIN_THIS{nullptr}, *UNPIN_ALL{nullptr};
+  QMenu* mMenu{nullptr};
 };
 
 #endif  // NAVIGATIONEXTOOLBAR_H
