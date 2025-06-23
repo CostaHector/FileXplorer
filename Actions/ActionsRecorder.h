6fb65fc4ecfaf853541411a794ca7cf079351781
CostaHector <hancosta@outlook.com>
2025-06-23 23:49:33 +0800

    Version 60.6.6 Actions Searcher now support. Help you quick find action

---
+++ Actions/ActionsRecorder.h
@@ -0,0 +1,38 @@
+﻿#ifndef ACTIONSRECORDER_H
+#define ACTIONSRECORDER_H
+
+#include <QMap>
+#include <QAction>
+#include <QToolBar>
+#include <QMenu>
+#include <QMenuBar>
+#include <QActionGroup>
+
+class ActionsSearcher;
+class ActionsRecorder {
+ public:
+  friend class ActionsSearcher;
+  static ActionsRecorder& GetInst() {
+    static ActionsRecorder inst;
+    return inst;
+  }
+  const QStringList& GetKeys() const {
+    if (isDirty) {
+      keys = mTextToActionMap.keys();
+      isDirty = false;
+    }
+    return keys;
+  }
+  int FromToolbar(QToolBar* tb);
+  int FromMenu(QMenu* menu);
+  int FromMenuBar(QMenuBar* mb);
+  int FromActionGroup(const QActionGroup* actGrp);
+
+ private:
+  ActionsRecorder() = default;
+  QMap<QString, QAction*> mTextToActionMap;
+  mutable QStringList keys;
+  mutable bool isDirty{true};
+};
+
+#endif  // ACTIONSRECORDER_H
