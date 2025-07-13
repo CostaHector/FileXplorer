e0f10dd74e7eb433fa94063be577de18fcee65d3
CostaHector <hancosta@outlook.com>
2025-07-13 11:37:46 +0800

    Thumbnail create enhancement

--- Tools/ThumbnailProcesser.h
+++ Tools/ThumbnailProcesser.h
@@ -5,13 +5,21 @@
 
 class ThumbnailProcesser {
  public:
+  static const QList<int> mAllowedPixelList;
+  static constexpr int SAMPLE_PERIOD_MIN{1}, SAMPLE_PERIOD_MAX{300 + 1};  // [1, 300)
+  static bool IsDimensionXValid(int dimensionX);
+  static bool IsDimensionYValid(int dimensionY);
+  static bool IsWidthPixelAllowed(int widthPixel);
+  static bool IsSamplePeriodAllowed(int samplePeriod);
+
   static bool IsImageAnThumbnail(const QString& imgAbsPath);
   static bool IsImageNameLooksLikeThumbnail(const QString& imgBaseName);
-  static int CreateThumbnailImages(int dimensionX, int dimensionY, int widthPx, const QStringList& files, const bool isJpg=true, const int timePeriod = 20);
+  static int CreateThumbnailImages(const QStringList& files, int dimensionX, int dimensionY, int widthPx, const int timePeriod = 20, const bool isJpg = true);
 
   explicit ThumbnailProcesser(bool skipIfImgAlreadyExist = true);
   int operator()(const QString& rootPath, int beg = 0, int end = 1);
   QStringList mErrImg;
+
  private:
   static std::pair<int, int> GetThumbnailDimension(const QString& imgBaseName);
   bool mSkipImageIfAlreadyExist;
