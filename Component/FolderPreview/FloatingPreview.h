7c7ce24ddf22be105f493868f4018d35e5151bed
ariel <costahan@gmail.com>
2025-08-09 17:15:38 +0800

    Version 60.8.3 Refactor Floating Toolbar on Floating Preview

--- Component/FolderPreview/FloatingPreview.h
+++ Component/FolderPreview/FloatingPreview.h
@@ -1,41 +1,23 @@
 ﻿#ifndef FLOATINGPREVIEW_H
 #define FLOATINGPREVIEW_H
 
-#include "Component/ReorderableToolBar.h"
-#include "View/ItemView.h"
-#include "Model/FloatingModels.h"
+#include "ImgVidOthWid.h"
 #include "ClickableTextBrowser.h"
-#include "Tools/WidgetReorderHelper.h"
-#include <QSplitter>
 #include <QStackedWidget>
-#include <QAction>
-#include <QActionGroup>
-#include <QToolButton>
-#include <QToolBar>
 #include <QSqlRecord>
-#include <QVector>
 
 class FloatingPreview : public QStackedWidget {
- public:
+public:
   enum class PANE_TYPE : int { BEGIN = 0, DETAIL = BEGIN, IMG_VID_OTH, BUTT };
   FloatingPreview(const QString& memoryName, QWidget* parent = nullptr);
 
   void ReadSettings();
   void SaveSettings();
-  void SaveState();
 
   void operator()(const QSqlRecord& record, const QString& imgHost, const int imgHeight);  // cast view
   void operator()(const QString& pth);                                                     // file system view
   void operator()(const QString& name, const QString& pth);                                // scene view
-  void UpdateImgs(const QString& name, const QStringList& imgPthLst);
-  void UpdateVids(const QStringList& dataLst);
-  void UpdateOthers(const QStringList& dataLst);
 
-  inline bool NeedUpdate(const QString& lastName) const { return !lastName.isEmpty() && (mLastName.isEmpty() || mLastName != lastName); }
-  inline bool NeedUpdateDetail() const { return m_curIndex == PANE_TYPE::DETAIL; }
-  inline bool NeedUpdateImgs() const { return m_curIndex == PANE_TYPE::IMG_VID_OTH && m_bImgVisible; }
-  inline bool NeedUpdateVids() const { return m_curIndex == PANE_TYPE::IMG_VID_OTH && m_bVidVisible; }
-  inline bool NeedUpdateOthers() const { return m_curIndex == PANE_TYPE::IMG_VID_OTH && m_bOthVisible; }
   inline void BeforeDisplayAFileDetail() {
     if (m_curIndex != PANE_TYPE::DETAIL) {
       setCurrentIndex((int)PANE_TYPE::DETAIL);
@@ -54,42 +36,27 @@ class FloatingPreview : public QStackedWidget {
     m_curIndex = static_cast<PANE_TYPE>(index);
     QStackedWidget::setCurrentIndex(index);
   }
-  void subscribe();
+  inline bool NeedUpdate(const QString& lastName) const { return !lastName.isEmpty() && (mLastName.isEmpty() || mLastName != lastName); }
 
- private:
-  void onImgBtnClicked(bool checked);
-  void onVidBtnClicked(bool checked);
-  void onOthBtnClicked(bool checked);
-  void onTypesBtnClicked(bool checked = true);
-  using MediaBtnHandlerFunc = void (FloatingPreview::*)(bool);
-  static constexpr MediaBtnHandlerFunc MEDIA_HANDLERS_MAP[(int)PREVIEW_ITEM_TYPE::BUTT]{
-      &FloatingPreview::onImgBtnClicked,  //
-      &FloatingPreview::onVidBtnClicked,  //
-      &FloatingPreview::onOthBtnClicked   //
-  };
-  void onImgVidOthActTriggered(const QAction* pAct);
+  // quick access
+  void UpdateImgs(const QString& name, const QStringList& imgPthLst) {
+    mLastName = name;
+    setWindowTitle(mLastName);
+    mImgVidOtherPane->UpdateImgs(name, imgPthLst);
+  }
+  void UpdateVids(const QStringList& dataLst) {
+    mImgVidOtherPane->UpdateVids(dataLst);
+  }
+  void UpdateOthers(const QStringList& dataLst) {
+    mImgVidOtherPane->UpdateOthers(dataLst);
+  }
 
-  bool onReorder(int fromIndex, int destIndex);
+private:
+  QString mLastName;
+  PANE_TYPE m_curIndex{PANE_TYPE::DETAIL};
 
   ClickableTextBrowser* mDetailsPane{nullptr};
-
-  ImgsModel* mImgModel{nullptr};
-  VidsModel* mVidsModel{nullptr};
-  OthersModel* mOthModel{nullptr};
-  ItemView *mImgTv{nullptr}, *mVidTv{nullptr}, *mOthTv{nullptr};
-  QAction *_IMG_ACT{nullptr}, *_VID_ACT{nullptr}, *_OTH_ACT{nullptr};
-  bool m_bImgVisible{true}, m_bVidVisible{true}, m_bOthVisible{true};
-  ReorderableToolBar* mTypeToDisplayTB{nullptr};
-  PANE_TYPE m_curIndex{PANE_TYPE::DETAIL};
-  QSplitter* mImgVidOtherSplitter{nullptr};
-
-  QString mLastName;
-  const QString mMemoryName;
-  QVector<int> mMediaSequence{
-      (int)PREVIEW_ITEM_TYPE::IMG,  //
-      (int)PREVIEW_ITEM_TYPE::VID,  //
-      (int)PREVIEW_ITEM_TYPE::OTH   //
-  };
+  ImgVidOthWid* mImgVidOtherPane{nullptr};
 };
 
 #endif  // FLOATINGPREVIEW_H
