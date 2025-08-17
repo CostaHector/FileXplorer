d72ee8bdc028b3573abcc1fbaef1f22b8352644c
ariel <costahan@gmail.com>
2025-08-17 11:44:00 +0800

    Version 60.9.1 Fix default open path contains special char error

--- main.cpp
+++ main.cpp
@@ -10,37 +10,37 @@
 #ifdef RUN_MAIN_FILE
 
 int main(int argc, char* argv[]) {
-  if (argc > 1) {
-    qInfo("argc[%d]. argv[%s, ...].", argc, argv[1]);
-  } else {
-    qInfo("argc[%d].", argc);
-  }
-
   if (!CreateUserPath()) {
     return -1;
   }
 
-  QApplication a(argc, argv);
+  QApplication app{argc, argv};
 #ifdef QT_DEBUG
   SetQtDebugMessagePattern();
 #else
-LogHandler logModule;
-if (!logModule.IsLogModuleOk()) {
-  return -1;
-}
-logModule.subscribe();
+  LogHandler logModule;
+  if (!logModule.IsLogModuleOk()) {
+    return -1;
+  }
+  logModule.subscribe();
 #endif
+  if (argc > 1) {
+    qInfo("argc[%d]. argv[1]=%s.", argc, argv[1]);
+  } else {
+    qInfo("argc[%d].", argc);
+  }
 
+  const QStringList& args = app.arguments();
   QTranslator translator;  // cannot define in local. will be release.
   if (PreferenceSettings().value(MemoryKey::LANGUAGE_ZH_CN.name, MemoryKey::LANGUAGE_ZH_CN.v).toBool()) {
     LoadCNLanguagePack(translator);
   }
 
-  FileXplorer fileExplorer{argc, argv, nullptr};
+  FileXplorer fileExplorer{args, nullptr};
   FileExplorerEvent::GetFileExlorerEvent(fileExplorer.m_fsPanel->m_fsModel, fileExplorer.m_fsPanel, fileExplorer.m_statusBar);
   ExtraViewVisibilityControl extraViewVisibility{fileExplorer.centralWidget()};
   fileExplorer.show();
-  a.exec();
+  app.exec();
   return 0;
 }
 #endif
