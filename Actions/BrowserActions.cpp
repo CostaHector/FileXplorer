499b6d6cc1a40c01c73b4209ae4b28cbfb3924bd
ariel <costahan@gmail.com>
2025-08-21 19:05:16 +0800

    Version 60.9.6 SelectionsRangeHelperTest/Cut/Copy/Drop Zone Visible

--- Actions/BrowserActions.cpp
+++ Actions/BrowserActions.cpp
@@ -3,6 +3,7 @@
 
 BrowserActions::BrowserActions(QObject *parent) : QObject{parent} {
   SEARCH_CUR_TEXT = new QAction{QIcon{":img/SEARCH"}, "Search Current Text", this};
+  SEARCH_CUR_TEXT->setShortcut(QKeySequence(Qt::AltModifier | Qt::Key_1));
   SEARCH_CUR_TEXT->setToolTip(QString{"<b>%1 (%2)</b><br/>Search for currently selected text in database"}//
                                   .arg(SEARCH_CUR_TEXT->text(), SEARCH_CUR_TEXT->shortcut().toString()));
 
@@ -16,6 +17,7 @@ BrowserActions::BrowserActions(QObject *parent) : QObject{parent} {
                                        .arg(ADVANCED_TEXT_SEARCH->text(), ADVANCED_TEXT_SEARCH->shortcut().toString()));
 
   CLEAR_ALL_SELECTIONS = new QAction{QIcon{":img/SELECT_NONE"}, "Clear All Selections", this};
+  CLEAR_ALL_SELECTIONS->setShortcut(QKeySequence(Qt::AltModifier | Qt::Key_2));
   CLEAR_ALL_SELECTIONS->setToolTip(QString{"<b>%1 (%2)</b><br/>Clear all text selections in current browser"}//
                                        .arg(CLEAR_ALL_SELECTIONS->text(), CLEAR_ALL_SELECTIONS->shortcut().toString()));
 
@@ -24,11 +26,16 @@ BrowserActions::BrowserActions(QObject *parent) : QObject{parent} {
                               .arg(EDITOR_MODE->text(), EDITOR_MODE->shortcut().toString()));
   EDITOR_MODE->setCheckable(true);
   EDITOR_MODE->setChecked(false);
+
+  COPY_SELECTED_TEXT = new QAction{QIcon{":img/COPY_TEXT"}, "Copy Selected Text", this};
+  COPY_SELECTED_TEXT->setShortcut(QKeySequence(Qt::AltModifier | Qt::Key_3));
 }
 
 QMenu* BrowserActions::GetSearchInDBMenu(QWidget* parent) {
   QMenu* pMenu = new (std::nothrow) QMenu{"Browser search menu", parent};
   CHECK_NULLPTR_RETURN_NULLPTR(pMenu)
+  pMenu->addAction(COPY_SELECTED_TEXT);
+  pMenu->addSeparator();
   pMenu->addAction(SEARCH_CUR_TEXT);
   pMenu->addAction(SEARCH_MULTIPLE_TEXTS);
   pMenu->addAction(ADVANCED_TEXT_SEARCH);
@@ -45,6 +52,7 @@ QToolBar* BrowserActions::GetSearchInDBToolbar(QWidget* parent) {
   floatingSearchTb->addAction(SEARCH_MULTIPLE_TEXTS);
   floatingSearchTb->addAction(ADVANCED_TEXT_SEARCH);
   floatingSearchTb->addSeparator();
+  floatingSearchTb->addAction(COPY_SELECTED_TEXT);
   floatingSearchTb->addAction(EDITOR_MODE);
   floatingSearchTb->addSeparator();
   floatingSearchTb->addAction(CLEAR_ALL_SELECTIONS);
--- Tools/SelectionsRangeHelper.h
+++ Tools/SelectionsRangeHelper.h
@@ -6,8 +6,8 @@
 
 struct SelectionsRangeHelper {
 public:
-  bool contain(const QString& rootpath, const QModelIndex& index) const {
-    return currentPath == rootpath && indexesSet.contains(index);
+  bool contains(const QString& rootpath, int row) const {
+    return currentPath == rootpath && indexesSet.contains(row);
   }
 
   void clear() {
@@ -17,34 +17,43 @@ public:
   }
 
   void Set(const QString& rootpath, const QModelIndexList& rhs) {
+    clear();
     currentPath = rootpath;
+
     if (rhs.isEmpty()) {return;}
-    int begin = 0;
-    int end = 0;
+
+    for (const QModelIndex& ind: rhs) {
+      indexesSet.insert(ind.row());
+    }
+
+    if (rhs.size() == rhs.back().row() - rhs.front().row() + 1) { // [topRow, bottomRow]
+      topLeft2BottomLeftLst.append({rhs.front().row(), rhs.back().row()});
+      return;
+    }
+
+    int top = 0;
+    int bottom = 0; // [top, bottom]
     for (int i = 1; i < rhs.size(); ++i) {
       const QModelIndex& current = rhs[i];
-      if (current.row() == rhs[end].row() + 1) {
-        end = i;
+      if (current.row() == rhs[bottom].row() + 1) {
+        bottom = i;
       } else {
-        topLeft2BottomLeftLst.append({rhs[begin], rhs[end]});
-        begin = i;
-        end = i;
+        topLeft2BottomLeftLst.append({rhs[top].row(), rhs[bottom].row()});
+        top = i;
+        bottom = i;
       }
     }
-    topLeft2BottomLeftLst.append({rhs[begin], rhs[end]});
-
-    QSet<QModelIndex> indexesAppend {rhs.cbegin(), rhs.cend()};
-    indexesSet.swap(indexesAppend);
+    topLeft2BottomLeftLst.append({rhs[top].row(), rhs[bottom].row()});
   }
 
-  QList<std::pair<QModelIndex, QModelIndex>> GetTopBottomRange() const {
+  QList<std::pair<int, int>> GetTopBottomRange() const {
     return topLeft2BottomLeftLst;
   }
 
 private:
   QString currentPath;
-  QList<std::pair<QModelIndex, QModelIndex>> topLeft2BottomLeftLst;
-  QSet<QModelIndex> indexesSet;
+  QList<std::pair<int, int>> topLeft2BottomLeftLst; // [front, back] contain endpoint
+  QSet<int> indexesSet;
 };
 
 #endif // SELECTIONSRANGEHELPER_H
