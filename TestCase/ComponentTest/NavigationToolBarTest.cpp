6373cd9a49e6f6b4a44277ebbf690b4c78f549c7
ariel <costahan@gmail.com>
2025-09-17 10:03:09 +0800

    Version 61.6.7 Testcase NavigationExToolBarTest added

---
+++ TestCase/ComponentTest/NavigationToolBarTest.cpp
@@ -0,0 +1,34 @@
+// #include <QtTest/QtTest>
+// #include <QTestEventList>
+// #include <QSignalSpy>
+
+// #include "PlainTestSuite.h"
+// #include "Logger.h"
+// #include "MemoryKey.h"
+// #include "BeginToExposePrivateMember.h"
+// #include "NavigationToolBar.h"
+// #include "EndToExposePrivateMember.h"
+
+// #include <QDir>
+// #include <QDirIterator>
+
+// Q_DECLARE_METATYPE(SearchTools::SearchModeE)
+
+// class NavigationToolBarTest : public PlainTestSuite {
+//   Q_OBJECT
+// private slots:
+//   void initTestCase() {
+//     qRegisterMetaType<SearchTools::SearchModeE>("SearchTools::SearchModeE");
+//     Configuration().clear();
+//   }
+
+//   void cleanupTestCase() {
+//     Configuration().clear();
+//   }
+
+//   void test_initialState() {
+//   }
+// };
+
+// #include "NavigationToolBarTest.moc"
+// REGISTER_TEST(NavigationToolBarTest, false)
