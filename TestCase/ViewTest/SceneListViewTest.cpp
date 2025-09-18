904ed78e0a512bdda27c44bda06866b7e448b4dc
Ariel <hancosta@outlook.com>
2025-09-18 11:51:20 -0400

    TestCase under views/ placeholder files

---
+++ TestCase/ViewTest/SceneListViewTest.cpp
@@ -0,0 +1,37 @@
+#include <QtTest/QtTest>
+#include "PlainTestSuite.h"
+#include "OnScopeExit.h"
+#include <QTestEventList>
+#include <QSignalSpy>
+
+#include "Logger.h"
+#include "MemoryKey.h"
+#include "BeginToExposePrivateMember.h"
+#include "SceneListView.h"
+#include "EndToExposePrivateMember.h"
+
+#include <QDir>
+#include <QDirIterator>
+
+Q_DECLARE_METATYPE(QDir::Filters)
+Q_DECLARE_METATYPE(QDirIterator::IteratorFlag)
+
+class SceneListViewTest : public PlainTestSuite {
+  Q_OBJECT
+ public:
+ private slots:
+  void initTestCase() {
+    qRegisterMetaType<QDir::Filters>("QDir::Filters");
+    qRegisterMetaType<QDirIterator::IteratorFlag>("QDirIterator::IteratorFlag");
+    Configuration().clear();
+  }
+
+  void cleanupTestCase() { Configuration().clear(); }
+
+  void test_1() {
+
+  }
+};
+
+#include "SceneListViewTest.moc"
+REGISTER_TEST(SceneListViewTest, false)
