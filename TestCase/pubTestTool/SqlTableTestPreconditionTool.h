4cbbf7b953076508ab33b6383d4fa6a92f15cf2b
Ariel <hancosta@outlook.com>
2025-10-01 01:34:03 -0400

    Version 62.3.9 Testcase CastDBViewTest added

--- TestCase/pubTestTool/SqlTableTestPreconditionTool.h
+++ TestCase/pubTestTool/SqlTableTestPreconditionTool.h
@@ -1,6 +1,7 @@
 #ifndef SQLTABLETESTPRECONDITIONTOOL_H
 #define SQLTABLETESTPRECONDITIONTOOL_H
 #include <QSqlTableModel>
+#include "TDir.h"
 
 namespace SqlTableTestPreconditionTool {
 bool CheckIndexesDisplayRoleIgnoreOrder(const QSqlTableModel& model,  //
@@ -10,6 +11,9 @@ bool CheckIndexesDisplayRoleIgnoreOrder(const QSqlTableModel& model,  //
                                         const QModelIndexList& indexes,
                                         QList<int> expectsList, Qt::ItemDataRole role=Qt::DisplayRole);
 QModelIndexList GetIndexessAtOneRow(const QSqlTableModel& model, int rowBegin, int rowEnd, int column);
+
+bool CreateFileStructure(TDir& tDir);
+bool CreateFileStructurePsonFiles(TDir& tDir);
 }
 
 #endif  // SQLTABLETESTPRECONDITIONTOOL_H
