70e1ad32f4f85ad2d659f2a7860dd857c957f4ef
ariel <costahan@gmail.com>
2025-10-04 19:48:24 +0800

    Version 62.4.8 Duration of invalid file differs in windows and linux

---
+++ Component/Archiver.cpp
@@ -0,0 +1,108 @@
+﻿#include "Archiver.h"
+#include "MemoryKey.h"
+#include "DataFormatter.h"
+#include "StyleSheet.h"
+#include "PublicVariable.h"
+#include "PathTool.h"
+#include <QToolBar>
+
+Archiver::Archiver(QWidget* parent) : QMainWindow{parent} {
+  m_splitter = new (std::nothrow) QSplitter{this};
+  CHECK_NULLPTR_RETURN_VOID(m_splitter);
+  m_itemsTable = new (std::nothrow) CustomTableView{"ArchiverItemsTable", this};
+  CHECK_NULLPTR_RETURN_VOID(m_itemsTable);
+  m_archiverModel = new (std::nothrow) ArchiverModel{this};
+  CHECK_NULLPTR_RETURN_VOID(m_archiverModel);
+  m_thumbnailViewer = new (std::nothrow) ThumbnailImageViewer{"AchiveImagePreview", this};
+  CHECK_NULLPTR_RETURN_VOID(m_thumbnailViewer);
+
+  m_itemsTable->setModel(m_archiverModel);
+  m_splitter->addWidget(m_itemsTable);
+  m_splitter->addWidget(m_thumbnailViewer);
+  setCentralWidget(m_splitter);
+
+  m_ImageSizeHint = new (std::nothrow) QLabel{this};
+  CHECK_NULLPTR_RETURN_VOID(m_ImageSizeHint);
+
+  m_ImageSizeScale = new (std::nothrow) QSlider{Qt::Orientation::Horizontal, this};
+  CHECK_NULLPTR_RETURN_VOID(m_ImageSizeScale);
+  m_ImageSizeScale->setRange(0, IMAGE_SIZE::ICON_SIZE_CANDIDATES_N - 1);
+  m_ImageSizeScale->setValue(m_thumbnailViewer->GetCurImageSizeScale());
+
+  QToolBar* tb = new (std::nothrow) QToolBar("Image adjuster toolbar", this);
+  CHECK_NULLPTR_RETURN_VOID(tb);
+  tb->addWidget(m_ImageSizeScale);
+  tb->addWidget(m_ImageSizeHint);
+  addToolBar(Qt::ToolBarArea::TopToolBarArea, tb);
+
+  m_itemsTable->InitTableView();
+  UpdateWindowsSize();
+  setWindowTitle("QZ Archive");
+  setWindowIcon(QIcon(":img/COMPRESS_ITEM"));
+
+  subscribe();
+}
+
+bool Archiver::operator()(const QString& qzPath) {
+  if (!ArchiveFilesReader::isQZFile(qzPath)) {
+    LOG_W("Path[%s] is not a qz file", qPrintable(qzPath));
+    setWindowTitle(QString("ArchiveFilesPreview | [%1] not a qz file").arg(qzPath));
+    return false;
+  }
+
+  m_archiverModel->setRootPath(qzPath);
+  return true;
+}
+
+void Archiver::ChangeWindowTitle(const QString& name, const int& Bytes) {
+  setWindowTitle(QString("QZ Archive | %1 | %2").arg(name).arg(DataFormatter::formatFileSizeGMKB(Bytes)));
+}
+
+void Archiver::UpdateWindowsSize() {
+  m_splitter->restoreState(Configuration().value("ArchiverSplitterState", QByteArray()).toByteArray());
+}
+
+void Archiver::showEvent(QShowEvent* event) {
+  QMainWindow::showEvent(event);
+  StyleSheet::UpdateTitleBar(this);
+}
+
+void Archiver::closeEvent(QCloseEvent* event) {
+  Configuration().setValue("ArchiverSplitterState", m_splitter->saveState());
+  QMainWindow::closeEvent(event);
+}
+
+void Archiver::subscribe() {
+  connect(m_itemsTable->selectionModel(), &QItemSelectionModel::currentRowChanged, this, &Archiver::onSelectNewItemRow);
+  connect(m_ImageSizeScale, &QSlider::valueChanged, this, &Archiver::onSilderChangedUpdateImageScaledIndex);
+  connect(m_thumbnailViewer, &ThumbnailImageViewer::onImageScaledIndexChanged, this, &Archiver::setSliderValueAndLabelDisplayText);
+}
+
+bool Archiver::onSelectNewItemRow(const QModelIndex& current, const QModelIndex& /* previous */) {
+  if (!current.isValid()) {
+    m_thumbnailViewer->clearPixmap();
+    return false;
+  }
+  int newRow = current.row();
+  const QString& name = m_archiverModel->GetRelativeName(newRow);
+  const QByteArray& dataByteArray = m_archiverModel->GetByteArrayData(newRow);
+  ChangeWindowTitle(name, dataByteArray.size());
+  if (TYPE_FILTER::IMAGE_TYPE_SET.contains(PathTool::GetAsteriskDotFileExtension(name))) {
+    m_thumbnailViewer->setPixmapByByteArrayData(dataByteArray); // only update for images
+  }
+  return true;
+}
+
+void Archiver::setSliderValueAndLabelDisplayText(int scaleIndex) {
+  m_ImageSizeScale->setValue(scaleIndex);
+  m_ImageSizeHint->setText(IMAGE_SIZE::HumanReadFriendlySize(scaleIndex));
+}
+
+void Archiver::onSilderChangedUpdateImageScaledIndex(int scaleIndex) {
+  if (!m_thumbnailViewer->setIconSizeScaledIndex(scaleIndex)) { // to label updata size index
+    LOG_D("Invalid image scaled index[%d]", scaleIndex);
+    return;
+  }
+  setSliderValueAndLabelDisplayText(scaleIndex); // to qslider and label display
+  m_thumbnailViewer->refreshPixmapSize(); // to label let it refresh
+}
---
+++ Component/PropertiesWindow.cpp
@@ -0,0 +1,161 @@
+﻿#include "PropertiesWindow.h"
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+#include "PropertiesWindowActions.h"
+#include "TableFields.h"
+#include "FileSystemItemFilter.h"
+#include "MD5Calculator.h"
+#include "VidsDurationDisplayString.h"
+#include "MemoryKey.h"
+#include "StyleSheet.h"
+#include "DataFormatter.h"
+
+const QString PropertiesWindow::STRING_SPLITTER{60, '-'};
+
+PropertiesWindow::PropertiesWindow(QWidget* parent)  //
+    : QDialog{parent}                                //
+{
+  m_propertyTB = g_propertiesWindowAct().getPropertiesToolBar(this);
+
+  m_mainLo->addWidget(m_propertyTB);
+  m_mainLo->addWidget(m_propertiesInfoTextEdit);
+  setLayout(m_mainLo);
+
+  subscribe();
+  setWindowFlags(Qt::Window);  // show maximize, minimize button at title bar
+
+  layout()->setSpacing(0);
+  layout()->setContentsMargins(0, 0, 0, 0);
+  ReadSetting();
+  setWindowIcon(QIcon(":img/PROPERTIES"));
+}
+
+bool PropertiesWindow::UpdateMessage() {
+  QString propertiesMsg;
+  if (g_propertiesWindowAct().SHOW_FILES_SIZE->isChecked()) {
+    if (m_commonInfomation.isEmpty()) {
+      InitCommonInfo();
+    }
+    propertiesMsg += STRING_SPLITTER;
+    propertiesMsg += m_commonInfomation;
+    propertiesMsg += "<br/>\n";
+  }
+  if (g_propertiesWindowAct().SHOW_VIDS_DURATION->isChecked()) {
+    if (m_durations.isEmpty()) {
+      InitDurationInfo();
+    }
+    propertiesMsg += STRING_SPLITTER;
+    propertiesMsg += m_durations;
+    propertiesMsg += "<br/>\n";
+  }
+  if (g_propertiesWindowAct().SHOW_FILES_MD5->isChecked()) {
+    if (m_fileIdentifier.isEmpty()) {
+      InitFileIndentifierInfo();
+    }
+    propertiesMsg += STRING_SPLITTER;
+    propertiesMsg += m_fileIdentifier;
+  }
+  m_propertiesInfoTextEdit->setHtml(propertiesMsg);
+  return true;
+}
+
+void PropertiesWindow::InitCommonInfo() {
+  const auto& itemStatic = FileSystemItemFilter::ItemCounter(m_items);
+  const QString sizeMsg = DataFormatter::formatFileSizeWithBytes(itemStatic.fileSize);
+  m_commonInfomation = QString("Contents: %1 file(s), %2 folder(s).<br/>\n").arg(itemStatic.fileCnt).arg(itemStatic.folderCnt);
+  m_commonInfomation += QString("Size: %3").arg(sizeMsg);
+}
+
+void PropertiesWindow::InitDurationInfo() {
+  const QStringList& mp4Files = FileSystemItemFilter::MP4Out(m_items);
+  m_durations = VidsDurationDisplayString::DisplayVideosDuration(mp4Files);
+}
+
+void PropertiesWindow::InitFileIndentifierInfo() {
+  const QStringList& files = FileSystemItemFilter::FilesOut(m_items);
+  m_fileIdentifier = MD5Calculator::DisplayFilesMD5(files);
+}
+
+bool PropertiesWindow::operator()(const QStringList& items) {
+  m_items = items;
+  setWindowTitle(QString("Property | [%1] item(s)").arg(m_items.size()));
+  if (m_items.isEmpty()) {
+    m_propertiesInfoTextEdit->setPlainText("Nothing selected");
+    return true;
+  }
+  m_commonInfomation.clear();
+  m_durations.clear();
+  m_fileIdentifier.clear();
+
+  if (g_propertiesWindowAct().SHOW_FILES_SIZE->isChecked()) {
+    InitCommonInfo();
+  }
+
+  if (g_propertiesWindowAct().SHOW_VIDS_DURATION->isChecked()) {
+    InitDurationInfo();
+  }
+
+  if (g_propertiesWindowAct().SHOW_FILES_MD5->isChecked()) {
+    InitFileIndentifierInfo();
+  }
+
+  UpdateMessage();
+  return true;
+}
+
+bool PropertiesWindow::operator()(const QSqlTableModel* model, const QTableView* tv) {
+  if (model == nullptr || tv == nullptr) {
+    LOG_C("model is nullptr");
+    return false;
+  }
+
+  m_commonInfomation = "not available";
+  m_durations = "not available";
+  m_fileIdentifier = "not available";
+
+  if (g_propertiesWindowAct().SHOW_FILES_SIZE->isChecked()) {
+    qint64 totalSz = 0;
+    const QModelIndexList& selIdxs = tv->selectionModel()->selectedRows();
+    const QModelIndex rootIndex = tv->rootIndex();
+    for (const QModelIndex& idx : selIdxs) {
+      const QModelIndex szInd = model->index(idx.row(), MOVIE_TABLE::Size, rootIndex);
+      totalSz += model->QSqlTableModel::data(szInd, Qt::ItemDataRole::DisplayRole).toLongLong();
+    }
+    m_commonInfomation = QString("Contents: %1 file(s), %2 folder(s).<br/>\n").arg(selIdxs.size()).arg(0);
+    const QString sizeMsg = DataFormatter::formatFileSizeWithBytes(totalSz);
+    m_commonInfomation += QString("Size: %3").arg(sizeMsg);
+  }
+
+  UpdateMessage();
+  return true;
+}
+
+void PropertiesWindow::ReadSetting() {
+  if (Configuration().contains("PropertiesWindowGeometry")) {
+    restoreGeometry(Configuration().value("PropertiesWindowGeometry").toByteArray());
+  } else {
+    setGeometry(DEFAULT_GEOMETRY);
+  }
+  m_propertiesInfoTextEdit->setFont(StyleSheet::TEXT_EDIT_FONT);
+}
+
+void PropertiesWindow::showEvent(QShowEvent* event) {
+  QDialog::showEvent(event);
+  StyleSheet::UpdateTitleBar(this);
+}
+
+void PropertiesWindow::closeEvent(QCloseEvent* event) {
+  Configuration().setValue("PropertiesWindowGeometry", saveGeometry());
+  QDialog::closeEvent(event);
+}
+
+void PropertiesWindow::subscribe() {
+  connect(g_propertiesWindowAct().SHOW_FILES_SIZE, &QAction::triggered, this, &PropertiesWindow::UpdateMessage);
+  connect(g_propertiesWindowAct().SHOW_VIDS_DURATION, &QAction::triggered, this, &PropertiesWindow::UpdateMessage);
+  connect(g_propertiesWindowAct().SHOW_FILES_MD5, &QAction::triggered, this, &PropertiesWindow::UpdateMessage);
+}
+
