560f87c0c907b7fa8c5606fe693173afe58af3a6
ariel <costahan@gmail.com>
2025-10-10 09:13:42 +0800

    Fix expect mount point available path expectation in testcase

--- TestCase/DbManagerTest/MountHelperTest.cpp
+++ TestCase/DbManagerTest/MountHelperTest.cpp
@@ -13,17 +13,17 @@ class MountHelperTest : public PlainTestSuite {
 
   void mountPath_tableName_bidirection_conversion_ok() {
     using namespace MountPathTableNameMapper;
-    QCOMPARE(toTableName(toMountPath("C__home_DISKS_DD2T4")), "C__home_DISKS_DD2T4");
+    QCOMPARE(toTableName(toMountPath("C__DISKS_DD2T4")), "C__DISKS_DD2T4");
     QCOMPARE(toTableName(toMountPath("_mnt_DISKS_DD2T4")), "_mnt_DISKS_DD2T4");
 
-    QCOMPARE(toMountPath(toTableName("C:/home/DISKS/DD2T4")), "C:/home/DISKS/DD2T4");
+    QCOMPARE(toMountPath(toTableName("C:/DISKS/DD2T4")), "C:/DISKS/DD2T4");
     QCOMPARE(toMountPath(toTableName("/mnt/DISKS/DD2T4")), "/mnt/DISKS/DD2T4");
 
     bool ok = false;
-    QCOMPARE(toTableName("C:/home/DISKS/DD2T4", &ok), "C__home_DISKS_DD2T4");
+    QCOMPARE(toTableName("C:/DISKS/DD2T4", &ok), "C__DISKS_DD2T4");
     QVERIFY(ok);
 
-    QCOMPARE(toMountPath("C__home_DISKS_DD2T4", &ok), "C:/home/DISKS/DD2T4");
+    QCOMPARE(toMountPath("C__DISKS_DD2T4", &ok), "C:/DISKS/DD2T4");
     QVERIFY(ok);
 
     QCOMPARE(toTableName("/mnt/DISKS/DD2T4", &ok), "_mnt_DISKS_DD2T4");
@@ -38,12 +38,12 @@ class MountHelperTest : public PlainTestSuite {
     // invalid below
     QCOMPARE(toMountPath("invalid_name", &ok), "");
     QVERIFY(!ok);
-    QCOMPARE(toMountPath("D:/home/DISKS/DD2T4", &ok), "");
+    QCOMPARE(toMountPath("D:/DISKS/DD2T4", &ok), "");
     QVERIFY(!ok);
 
     QCOMPARE(toTableName("invalid_name", &ok), "");
     QVERIFY(!ok);
-    QCOMPARE(toTableName("D__home_DISKS_DD2T4", &ok), "");
+    QCOMPARE(toTableName("D__DISKS_DD2T4", &ok), "");
     QVERIFY(!ok);
 
     const QStringList tablesNamesFromMountPath = CandidateTableNamesList();
