5228ccaba7ebd94c90ee189512d1f90095aa12e8
Costa <CostaHan@outlook.com>
2026-08-12 08:41:42 +0800

    Replace Renamer now support longest common prefix

--- Component/RenameWidgets/RenameWidget_Replace.cpp
+++ Component/RenameWidgets/RenameWidget_Replace.cpp
@@ -3,6 +3,41 @@
 #include "RenamerKey.h"
 #include "Configuration.h"
 #include "PublicMacro.h"
+#include "PathTool.h"
+
+#include <QLineEdit>
+#include <algorithm>
+
+#define OLD_STR_DEFAULT_MODE_MAPPING \
+OLD_STR_DEFAULT_MODE_ITEM(KEEP_EMPTY, 0, "keep empty")\
+    OLD_STR_DEFAULT_MODE_ITEM(LONGEST_COMMON_PREFIX, 1, "longest common prefix")\
+    OLD_STR_DEFAULT_MODE_ITEM(SHORTEST_ONE, 2, "shortest one")\
+    OLD_STR_DEFAULT_MODE_ITEM(LONGEST_ONE, 3, "longest one")\
+
+    enum OldStrDefaultMode{
+      OLD_STR_DEFAULT_MODE_BEGIN,
+#define OLD_STR_DEFAULT_MODE_ITEM(enumName, enumVal, enumStr) OLD_STR_DEFAULT_MODE_##enumName = enumVal,
+      OLD_STR_DEFAULT_MODE_MAPPING
+#undef OLD_STR_DEFAULT_MODE_ITEM
+          OLD_STR_DEFAULT_MODE_BUTT
+    };
+
+bool isOldStrDefaultModeValid(int oldStrDefaultMode) {
+  return OLD_STR_DEFAULT_MODE_BEGIN <= oldStrDefaultMode && oldStrDefaultMode < OLD_STR_DEFAULT_MODE_BUTT;
+}
+
+OldStrDefaultMode GetOldStrDefaultModeFromMemory() {
+  bool bOk{false};
+  int oldStrDefaultMode = getConfig(RenamerKey::OLD_STR_DEFAULT_MODE_SELECT).toInt(&bOk);
+  if (!bOk || !isOldStrDefaultModeValid(oldStrDefaultMode)) {
+    return OLD_STR_DEFAULT_MODE_BEGIN;
+  }
+  return static_cast<OldStrDefaultMode>(oldStrDefaultMode);;
+}
+
+RenameWidget_Replace::~RenameWidget_Replace() {
+  Configuration().setValue(RenamerKey::OLD_STR_DEFAULT_MODE_SELECT.name, m_oldStrDefaultModeCB->currentIndex());
+}
 
 QToolBar* RenameWidget_Replace::InitControlTB() {
   QToolBar* replaceControl = new (std::nothrow) QToolBar{"replace tb", this};
@@ -16,6 +51,8 @@ QToolBar* RenameWidget_Replace::InitControlTB() {
   replaceControl->addWidget(m_oldStrCB);
   replaceControl->addWidget(pNewLabel);
   replaceControl->addWidget(m_newStrCB);
+  replaceControl->addSeparator();
+  replaceControl->addWidget(m_oldStrDefaultModeCB);
   replaceControl->addSeparator();
   replaceControl->addWidget(m_regexCB);
   replaceControl->addWidget(m_nameExtIndependent);
@@ -31,7 +68,55 @@ void RenameWidget_Replace::extraSubscribe() {
   connect(m_newStrCB, &QComboBox::currentTextChanged, this, &RenameWidget_Replace::OnlyTriggerRenameCore);
 }
 
-auto RenameWidget_Replace::InitExtraMemberWidget() -> void {
+QString GetOldStrDefault(const QStringList& oldStrList, OldStrDefaultMode oldStrDefaultMode) {
+  switch (oldStrDefaultMode) {
+    case OLD_STR_DEFAULT_MODE_KEEP_EMPTY: return "";
+    case OLD_STR_DEFAULT_MODE_LONGEST_COMMON_PREFIX: return PathTool::longestCommonPrefix(oldStrList);
+    case OLD_STR_DEFAULT_MODE_SHORTEST_ONE:
+      return oldStrList.isEmpty() ? "" : *std::min_element(oldStrList.begin(), oldStrList.end(),
+          [](const QString& a, const QString& b) { return a.length() < b.length(); });
+    case OLD_STR_DEFAULT_MODE_LONGEST_ONE:
+      return oldStrList.isEmpty() ? "" : *std::max_element(oldStrList.begin(), oldStrList.end(),
+          [](const QString& a, const QString& b) { return a.length() < b.length(); });
+    default:
+      return "";
+  }
+}
+
+bool RenameWidget_Replace::UpdateOldStrCbCurrentText() {
+  // return false when invalid or unchange
+  int oldStrDefaultMode = m_oldStrDefaultModeCB->currentIndex();
+  if (!isOldStrDefaultModeValid(oldStrDefaultMode)) {
+    LOG_E("oldStrDefaultMode[%d] out of range", oldStrDefaultMode);
+    return false;
+  }
+  OldStrDefaultMode defMode = static_cast<OldStrDefaultMode>(oldStrDefaultMode);
+  const QString beforeOldName = m_oldStrCB->currentText();
+  const QString afterOldName = GetOldStrDefault(mNames, defMode);
+  if (afterOldName == beforeOldName) {
+    return false;
+  }
+  m_oldStrCB->setCurrentText(afterOldName);
+  return true;
+}
+
+void RenameWidget_Replace::onOldStrDefaultModeChanged(int oldStrDefaultMode) {
+  if (!UpdateOldStrCbCurrentText()) {
+    return;
+  }
+  OnlyTriggerRenameCore();
+}
+
+const QStringList& GetOldStrDefaultModeCandidatesList() {
+  static const QStringList candidates{
+#define OLD_STR_DEFAULT_MODE_ITEM(enumName, enumVal, enumStr) enumStr,
+      OLD_STR_DEFAULT_MODE_MAPPING
+#undef OLD_STR_DEFAULT_MODE_ITEM
+  };
+  return candidates;
+}
+
+void RenameWidget_Replace::InitExtraMemberWidget() {
   m_oldStrCB = new (std::nothrow) QComboBox{this};
   CHECK_NULLPTR_RETURN_VOID(m_oldStrCB)
   m_oldStrCB->addItems(MultiLineStr2StrList(RenamerKey::REP_OLD_STR_LIST));
@@ -48,6 +133,16 @@ auto RenameWidget_Replace::InitExtraMemberWidget() -> void {
   m_newStrCB->setEditable(true);
   m_newStrCB->setCompleter(nullptr);
 
+  m_oldStrDefaultModeCB = new (std::nothrow) QComboBox{this};
+  {
+    CHECK_NULLPTR_RETURN_VOID(m_oldStrDefaultModeCB)
+    m_oldStrDefaultModeCB->addItems(GetOldStrDefaultModeCandidatesList());
+    m_oldStrDefaultModeCB->setCurrentIndex(GetOldStrDefaultModeFromMemory());
+    m_oldStrDefaultModeCB->setFocusPolicy(Qt::FocusPolicy::StrongFocus);
+    m_oldStrDefaultModeCB->setSizePolicy(QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Minimum);
+    m_oldStrDefaultModeCB->setEditable(false);
+  }
+
   m_regexCB = new (std::nothrow) QCheckBox{tr("Regex"), this};
   CHECK_NULLPTR_RETURN_VOID(m_regexCB)
   m_regexCB->setIcon(QIcon(":img/MATCH_REGEX"));
@@ -56,6 +151,13 @@ auto RenameWidget_Replace::InitExtraMemberWidget() -> void {
 }
 
 QStringList RenameWidget_Replace::RenameCore(const QStringList& replaceeList) {
+  if (!m_oldStrInited) {
+    // init lineedit only at first time. when lineedit editted by user. lineedit should not init
+    m_oldStrInited = true;
+    UpdateOldStrCbCurrentText();
+    using CURRENT_INDEX_CHANGED_MEMBER_FUNC = void (QComboBox::*)(int);
+    connect(m_oldStrDefaultModeCB, (CURRENT_INDEX_CHANGED_MEMBER_FUNC)&QComboBox::currentIndexChanged, this, &RenameWidget_Replace::onOldStrDefaultModeChanged);
+  }
   const QString& oldString = m_oldStrCB->currentText();
   const QString& newString = m_newStrCB->currentText();
   const bool regexEnable = m_regexCB->isChecked();
